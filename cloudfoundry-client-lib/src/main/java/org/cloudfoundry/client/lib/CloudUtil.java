54c43e7a6028571a5362d8dd38542ac1995001ce
AB Srinivasan <abyss@b20nine.com>
2011-10-31 16:49:36 -0700

    Add support for deploying apps from a directory instead of requiring a war: a: facilitates deploying exploded Java web apps - no extra copying involved b. groundwork for deploying non Java framework based apps
    Change-Id: I98359751647be97e5b426dba1ad658d98468eeba

--- cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudUtil.java
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudUtil.java
@@ -16,18 +16,41 @@
 
 package org.cloudfoundry.client.lib;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
+/**
+ * Some helper utilities used by the Cloud Foundry Java client.
+ *
+ * @author Ramnivas Laddad
+ * @author A.B.Srinivasan
+ *
+ */
 public class CloudUtil {
+
+        public static final int BUFFER_SIZE = 16 * 1024;
 
 	private static Double DEFAULT_DOUBLE = new Double(0.0);
 
 	private static Integer DEFAULT_INTEGER = new Integer(0);
 
 	private static Long DEFAULT_LONG = new Long(0);
+
+	private static final String HEX_CHARS = "0123456789ABCDEF";
+
+	private static final char EXTENSION_SEPARATOR = '.';
 
 	public static String parse(Object object) {
 		return parse(String.class, object);
@@ -72,4 +95,94 @@ public class CloudUtil {
 		}
 		return defaultValue;
 	}
+
+	public static void unpackWar(String warFile, String destDir) throws IOException {
+	    ZipInputStream zis = new ZipInputStream(new FileInputStream(warFile));
+	    try {
+	        ZipEntry entry;
+	        while ((entry = zis.getNextEntry()) != null) {
+	            if (entry.isDirectory()) {
+	                unpackDir(destDir, entry);
+	            } else {
+	                unpackFile(destDir, entry, zis);
+	            }
+	        }
+	    } finally {
+	        if (zis != null) {
+	            zis.close();
+	        }
+	    }
+	}
+
+	public static boolean isWar(String filePath) {
+	    return "war".equalsIgnoreCase(extension(filePath));
+	}
+
+    public static String computeSha1Digest(InputStream in) throws IOException {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        byte[] buffer = new byte[CloudUtil.BUFFER_SIZE];
+        while(true) {
+            int read = in.read(buffer);
+            if (read == -1) {
+                break;
+            }
+            digest.update(buffer, 0, read);
+        }
+        in.close();
+        return bytesToHex(digest.digest());
+    }
+
+	private static String bytesToHex(byte[] bytes) {
+	    if (bytes == null) {
+	        return null;
+	    }
+	    final StringBuilder hex = new StringBuilder(2 * bytes.length);
+	    for (final byte b : bytes) {
+	        hex.append(HEX_CHARS.charAt((b & 0xF0) >> 4)).append(HEX_CHARS.charAt((b & 0x0F)));
+	    }
+	    return hex.toString();
+	}
+
+	private static void unpackDir(String destDir, ZipEntry entry) {
+	    File dir = new File(destDir, entry.getName());
+	    dir.mkdir();
+	    if (entry.getTime() != -1) {
+	        dir.setLastModified(entry.getTime());
+	    }
+	}
+
+	private static void unpackFile(String destDir, ZipEntry entry, ZipInputStream zis) throws IOException {
+	    BufferedOutputStream dest = null;
+	    int count;
+	    byte data[] = new byte[BUFFER_SIZE];
+	    File destFile;
+	    try {
+	        destFile = new File(destDir, entry.getName());
+	        FileOutputStream fos = new FileOutputStream(destFile);
+	        dest = new BufferedOutputStream(fos, BUFFER_SIZE);
+	        while ((count = zis.read(data, 0, BUFFER_SIZE)) != -1) {
+	            dest.write(data, 0, count);
+	        }
+	        if (entry.getTime() != -1) {
+	            destFile.setLastModified(entry.getTime());
+	        }
+	    } finally {
+	        if (dest != null) {
+	            dest.flush();
+	            dest.close();
+	        }
+	    }
+	}
+
+	private static String extension(String filePath) {
+	    int dot = filePath.lastIndexOf(EXTENSION_SEPARATOR);
+	    return filePath.substring(dot + 1);
+	}
+
 }
