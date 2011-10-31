22c462ef0cbff17b5fc24c426e1335682062988f
AB Srinivasan <abyss@b20nine.com>
2011-10-31 16:49:36 -0700

    Add support for deploying apps from a directory instead of requiring a war: a: facilitates deploying exploded Java web apps - no extra copying involved b. groundwork for deploying non Java framework based apps
    Change-Id: I98359751647be97e5b426dba1ad658d98468eeba

--- cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudUtil.java
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudUtil.java
@@ -16,18 +16,36 @@
 
 package org.cloudfoundry.client.lib;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+/**
+ * Some helper utilities used by the Cloud Foundry Java client.
+ *
+ * @author Ramnivas Laddad
+ * @author A.B.Srinivasan
+ *
+ */
 public class CloudUtil {
+
+    public static final int BUFFER_SIZE = 16 * 1024;
 
 	private static Double DEFAULT_DOUBLE = new Double(0.0);
 
 	private static Integer DEFAULT_INTEGER = new Integer(0);
 
 	private static Long DEFAULT_LONG = new Long(0);
+
+	private static final String HEX_CHARS = "0123456789ABCDEF";
+
+	private static final char EXTENSION_SEPARATOR = '.';
 
 	public static String parse(Object object) {
 		return parse(String.class, object);
@@ -72,4 +90,45 @@ public class CloudUtil {
 		}
 		return defaultValue;
 	}
+
+	public static boolean isWar(String filePath) {
+	    return "war".equalsIgnoreCase(extension(filePath));
+	}
+
+	public static String computeSha1Digest(InputStream in) throws IOException {
+	    MessageDigest digest;
+	    try {
+	        digest = MessageDigest.getInstance("SHA-1");
+	    } catch (NoSuchAlgorithmException e) {
+	        throw new RuntimeException(e);
+	    }
+
+	    byte[] buffer = new byte[CloudUtil.BUFFER_SIZE];
+	    while(true) {
+	        int read = in.read(buffer);
+	        if (read == -1) {
+	            break;
+	        }
+	        digest.update(buffer, 0, read);
+	    }
+	    in.close();
+	    return bytesToHex(digest.digest());
+	}
+
+	private static String bytesToHex(byte[] bytes) {
+	    if (bytes == null) {
+	        return null;
+	    }
+	    final StringBuilder hex = new StringBuilder(2 * bytes.length);
+	    for (final byte b : bytes) {
+	        hex.append(HEX_CHARS.charAt((b & 0xF0) >> 4)).append(HEX_CHARS.charAt((b & 0x0F)));
+	    }
+	    return hex.toString();
+	}
+
+	private static String extension(String filePath) {
+	    int dot = filePath.lastIndexOf(EXTENSION_SEPARATOR);
+	    return filePath.substring(dot + 1);
+	}
+
 }
