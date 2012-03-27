8011ded88e5b61eba476657d3e344b365b8d7c2a
Thomas Risberg <trisberg@vmware.com>
2012-03-27 19:19:01 -0400

    Add Caldecott Java client
    - Add library module for Caldecott Java client
    
    - Add test module for integration tests for Caldecott Java client
    
    - Add new profiles to CloudFoundry Java client to exclude integration test by default
    
    - Add integration-test profile that executes all integration tests
    
    Change-Id: I70437d5ab807fa307275f7007b2b7f858e80a10e

---
+++ caldecott-java-lib/src/main/java/org/cloudfoundry/caldecott/client/TunnelHandler.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2009-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.caldecott.client;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cloudfoundry.caldecott.TunnelException;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Observable;
+
+/**
+ * The class responsible for handling the actual tunneling communications between a data access client and the
+ * Caldecott server app.
+ *
+ * @author Thomas Risberg
+ */
+public class TunnelHandler extends Observable {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	// configuration options
+	private final Socket socket;
+	private final TunnelFactory tunnelFactory;
+	private final TaskExecutor taskExecutor;
+
+	// variables to keep state for the tunnel setup
+	private Client client;
+	private Tunnel tunnel;
+
+
+	public TunnelHandler(Socket socket, TunnelFactory tunnelFactory, TaskExecutor taskExecutor) {
+		this.socket = socket;
+		this.tunnelFactory = tunnelFactory;
+		this.taskExecutor = taskExecutor;
+		try {
+			this.socket.setSoTimeout(0);
+		} catch (SocketException ignore) {}
+	}
+
+	public void start() {
+		client = new SocketClient(socket);
+		tunnel = tunnelFactory.createTunnel();
+		taskExecutor.execute(new Writer());
+		taskExecutor.execute(new Reader());
+		if (logger.isDebugEnabled()) {
+			logger.debug("Completed start of: " + this.getClass().getSimpleName() + " with " + countObservers() + " observers");
+		}
+	}
+
+	public void stop() {
+		try {
+			InputStream is = socket.getInputStream();
+			if (is != null) {
+				is.close();
+			}
+		} catch (IOException ignore) {}
+		try {
+			OutputStream os = socket.getOutputStream();
+			if (os != null) {
+				os.close();
+			}
+		} catch (IOException ignore) {}
+		try {
+			socket.close();
+		} catch (IOException e) {
+			logger.warn("Error while closing client socket" + e.getMessage());
+		}
+		if (logger.isDebugEnabled()) {
+			logger.debug("Closing tunnel: " + tunnel.toString());
+		}
+		tunnel.close();
+		if (logger.isDebugEnabled()) {
+			logger.debug("Notifying observers: " + countObservers());
+		}
+		setChanged();
+		notifyObservers("CLOSED");
+	}
+
+
+	private class Writer implements Runnable {
+
+		public void run() {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Starting new writer thread: " + this);
+			}
+			try {
+				while (client.isActive()) {
+					byte[] in = client.read();
+					if (in.length > 0) {
+						tunnel.write(in);
+					}
+				}
+			} catch (IOException e) {
+				throw new TunnelException("Error while processing streams", e);
+			}
+			stop();
+			if (logger.isDebugEnabled()) {
+				logger.debug("Completed writer thread for: " + this);
+			}
+		}
+
+	}
+
+	private class Reader implements Runnable {
+		public void run() {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Starting new reader thread: " + this);
+			}
+			boolean retry = false;
+			try {
+				while (client.isActive()) {
+					try {
+						byte[] out = tunnel.read(retry);
+						retry = false;
+						client.write(out);
+					} catch (HttpStatusCodeException hsce) {
+						if (hsce.getStatusCode().value() == 504) {
+							retry = true;
+							if (logger.isTraceEnabled()) {
+								logger.trace("Retrying tunnel read after receiving " + hsce.getStatusCode().value());
+							}
+						}
+						else if (hsce.getStatusCode().value() == 404) {
+							retry = false;
+							if (logger.isDebugEnabled()) {
+								logger.debug("Tunnel error - [" + hsce.getStatusCode().value() + "] " + hsce.getStatusText());
+							}
+						}
+						else if (hsce.getStatusCode().value() == 410) {
+							retry = false;
+							if (logger.isDebugEnabled()) {
+								logger.debug("Tunnel error - [" + hsce.getStatusCode().value() + "] " + hsce.getStatusText());
+							}
+						}
+						else {
+							logger.warn("Received HTTP Error: [" + hsce.getStatusCode().value() + "] " + hsce.getStatusText());
+							throw new TunnelException("Error while reading from tunnel", hsce);
+						}
+					}
+				}
+			} catch (IOException ioe) {
+				throw new TunnelException("Error while processing streams", ioe);
+			}
+			if (logger.isDebugEnabled()) {
+				logger.debug("Completed reader thread for: " + this);
+			}
+		}
+
+	}
+
+}
---
+++ caldecott-java-lib/src/main/java/org/cloudfoundry/caldecott/client/TunnelServer.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2009-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.caldecott.client;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cloudfoundry.caldecott.TunnelException;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ExecutorConfigurationSupport;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+
+/**
+ * A class responsible for starting and stopping the TunnelAcceptor based on the configuration options.
+ *
+ * @author Thomas Risberg
+ */
+public class TunnelServer {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	// configuration options
+	private final InetSocketAddress local;
+	private final TunnelFactory tunnelFactory;
+	private final TaskExecutor taskExecutor;
+
+	// variables to keep state for server
+	private final ServerSocket serverSocket;
+	private TunnelAcceptor acceptor;
+
+	public TunnelServer(InetSocketAddress local, TunnelFactory tunnelFactory) {
+		this(local, tunnelFactory, getDefaultThreadExecutor());
+	}
+
+	public TunnelServer(InetSocketAddress local, TunnelFactory tunnelFactory, TaskExecutor taskExecutor) {
+		this.local = local;
+		this.tunnelFactory = tunnelFactory;
+		this.taskExecutor = taskExecutor;
+		try {
+			this.serverSocket = new ServerSocket();
+			serverSocket.setReuseAddress(true);
+			serverSocket.bind(local);
+		} catch (IOException e) {
+			throw new TunnelException("Error configuring server socket", e);
+		}
+	}
+
+	public void start() {
+		logger.info("Starting server on " + local);
+		initializeTaskExecutor(taskExecutor);
+		synchronized (this) {
+			if (acceptor == null) {
+				this.acceptor = new TunnelAcceptor(serverSocket, tunnelFactory, taskExecutor);
+				acceptor.start();
+			}
+			else {
+				throw new TunnelException("Server already running.");
+			}
+		}
+	}
+
+	public void stop() {
+		logger.info("Stopping server on " + local);
+		synchronized (this) {
+			shutdownTaskExecutor(taskExecutor);
+			if (acceptor != null) {
+				acceptor.stop();
+				if (acceptor.isActive()) {
+					logger.info("Server is actively servicing connections, waiting for client to close");
+					while (acceptor.isActive()) {
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException ignore) {}
+					}
+					logger.info("Server on " + local + " is now stopped");
+				}
+			}
+			else {
+				throw new TunnelException("Server is not running.");
+			}
+		}
+	}
+
+	protected static void initializeTaskExecutor(TaskExecutor taskExecutor) {
+		if (taskExecutor instanceof ExecutorConfigurationSupport) {
+			((ExecutorConfigurationSupport)taskExecutor).initialize();
+		}
+	}
+
+	protected static void shutdownTaskExecutor(TaskExecutor taskExecutor) {
+		if (taskExecutor instanceof ExecutorConfigurationSupport) {
+			((ExecutorConfigurationSupport)taskExecutor).shutdown();
+		}
+	}
+
+	protected static TaskExecutor getDefaultThreadExecutor() {
+		ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+		te.setCorePoolSize(5);
+		te.setMaxPoolSize(10);
+		te.setQueueCapacity(100);
+		return te;
+	}
+}
