b7653402288d5fcd1aeb536bc6bb2123fc5368f0
Thomas Risberg <trisberg@vmware.com>
2012-07-25 17:19:12 -0400

    Adding UAA authentication when CC provides authentication end-point
    Change-Id: I6e07fa99eb1f6bd07e16ed5c42cd48163ee18969

---
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/oauth2/OauthClient.java
@@ -0,0 +1,75 @@
+package org.cloudfoundry.client.lib.oauth2;
+
+import org.cloudfoundry.client.lib.CloudFoundryException;
+import org.cloudfoundry.client.lib.util.CloudUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.CommonsClientHttpRequestFactory;
+import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitResourceDetails;
+import org.springframework.security.oauth2.common.AuthenticationScheme;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Client that can handle authentication against a UAA instance
+ *
+ * @author: Dave Syer
+ * @author: Thomas Risberg
+ */
+public class OauthClient {
+
+	private URL authorizationUrl;
+
+	private RestTemplate restTemplate;
+
+	public OauthClient(URL authorizationUrl) {
+		this.authorizationUrl = authorizationUrl;
+		this.restTemplate = new RestTemplate();
+		this.restTemplate.setRequestFactory(new CommonsClientHttpRequestFactory());
+	}
+
+	public OAuth2AccessToken getToken(String username, String password) {
+		OAuth2ProtectedResourceDetails resource = getImplicitResource();
+		Map<String, String> parameters = new LinkedHashMap<String, String>();
+		parameters.put("credentials", String.format("{\"username\":\"%s\",\"password\":\"%s\"}", username, password));
+		AccessTokenRequest request = new DefaultAccessTokenRequest();
+		request.setAll(parameters);
+		ImplicitAccessTokenProvider provider = new ImplicitAccessTokenProvider();
+		provider.setRestTemplate(restTemplate);
+		OAuth2AccessToken token = null;
+		try {
+			token = provider.obtainAccessToken(resource, request);
+		}
+		catch (OAuth2AccessDeniedException oauthEx) {
+			HttpStatus status = HttpStatus.valueOf(oauthEx.getHttpErrorCode());
+			CloudFoundryException cfEx = new CloudFoundryException(status, oauthEx.getMessage());
+			cfEx.setDescription(oauthEx.getSummary());
+			throw cfEx;
+		}
+		return token;
+	}
+
+	private ImplicitResourceDetails getImplicitResource() {
+		ImplicitResourceDetails resource = new ImplicitResourceDetails();
+		String clientId = "vmc";
+		resource.setClientId(clientId);
+		resource.setId(clientId);
+		resource.setClientAuthenticationScheme(AuthenticationScheme.header);
+		resource.setAccessTokenUri(authorizationUrl + "/oauth/authorize");
+		resource.setScope(Arrays.asList("read"));
+		String redirectUri = "http://uaa.cloudfoundry.com/redirect/vmc";
+		resource.setPreEstablishedRedirectUri(redirectUri);
+		resource.setUseCurrentUri(false);
+		return resource;
+	}
+
+}
