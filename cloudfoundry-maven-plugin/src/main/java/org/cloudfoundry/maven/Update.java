883dbfa03a231dbd59409e380ec2fbad22b1e3af
Ali Moghadam <alighm@gmail.com>
2013-01-10 17:44:27 -0800

    Adding domains to the given org during the following goals
     - Push Goal
    
     - Update Goal
    
    Change-Id: I7ba04aae239c01fdc3911dde4696579fe541ab15

--- cloudfoundry-maven-plugin/src/main/java/org/cloudfoundry/maven/Update.java
+++ cloudfoundry-maven-plugin/src/main/java/org/cloudfoundry/maven/Update.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2012 the original author or authors.
+ * Copyright 2009-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import org.cloudfoundry.maven.common.Assert;
  * Updates an application.
  *
  * @author Gunnar Hillert
+ * @author Ali Moghadam
  * @since 1.0.0
  *
  * @goal update
@@ -56,6 +57,11 @@ public class Update extends AbstractApplicationAwareCloudFoundryMojo {
 		getLog().info(String.format("Updating application '%s' and Deploying '%s'.", appName, path.getAbsolutePath()));
 
 		uploadApplication(getClient(), path, appName);
+
+		if (isCloudControllerV2(getClient())) {
+			getLog().debug("Updating domains for cc v2");
+			addDomains();
+		}
 
 		getLog().debug("Updating application memory");
 		getClient().updateApplicationMemory(appName, getMemory());
