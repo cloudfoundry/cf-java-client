cloudfoundry/cf-java-client

commit edcb7a845f5ccfd90dbad21bef9f6b0fff6cffbf

Make client deal with expired token transparently.
- Use OwnerPassword resource instead of implicit to match how it is done is cf
- Check if the token is about to expire and use refresh token if so.
- Remove unneeded unit tests

cloudfoundry-client-lib/pom.xml
@@ -19,7 +19,7 @@
 
 	<properties>
 		<spring.framework.version>3.0.7.RELEASE</spring.framework.version>
-		<spring.security.oauth.version>1.0.0.RC1</spring.security.oauth.version>
+		<spring.security.oauth.version>1.0.0.RELEASE</spring.security.oauth.version>
 	</properties>
 
 	<profiles>

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/oauth2/OauthClient.java
@@ -32,8 +32,8 @@ import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedExc
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.AccessTokenRequest;
 import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
-import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
-import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.security.oauth2.common.AuthenticationScheme;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.client.RestTemplate;
@@ -58,13 +58,10 @@ public class OauthClient {
 	}
 
 	public OAuth2AccessToken getToken(String username, String password) {
-		OAuth2ProtectedResourceDetails resource = getImplicitResource();
-		Map<String, String> parameters = new LinkedHashMap<String, String>();
-		parameters.put("credentials", String.format("{\"username\":\"%s\",\"password\":\"%s\"}", username, password));
-		AccessTokenRequest request = new DefaultAccessTokenRequest();
-		request.setAll(parameters);
-		ImplicitAccessTokenProvider provider = new ImplicitAccessTokenProvider();
-		provider.setRestTemplate(restTemplate);
+		OAuth2ProtectedResourceDetails resource = getResourceDetails(username, password);
+		AccessTokenRequest request = createAccessTokenRequest(username, password);
+		
+		ResourceOwnerPasswordAccessTokenProvider provider = new ResourceOwnerPasswordAccessTokenProvider();
 		OAuth2AccessToken token = null;
 		try {
 			token = provider.obtainAccessToken(resource, request);
@@ -77,11 +74,20 @@ public class OauthClient {
 		}
 		return token;
 	}
+	
+	public OAuth2AccessToken refreshToken(OAuth2AccessToken currentToken, String username, String password) {
+		OAuth2ProtectedResourceDetails resource = getResourceDetails(username, password);
+		AccessTokenRequest request = createAccessTokenRequest(username, password);
+
+		ResourceOwnerPasswordAccessTokenProvider provider = new ResourceOwnerPasswordAccessTokenProvider();
+		
+		return provider.refreshAccessToken(resource, currentToken.getRefreshToken(), request);
+	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void changePassword(String token, String oldPassword, String newPassword) {
+	public void changePassword(OAuth2AccessToken token, String oldPassword, String newPassword) {
 		HttpHeaders headers = new HttpHeaders();
-		headers.add(AUTHORIZATION_HEADER_KEY, token);
+		headers.add(AUTHORIZATION_HEADER_KEY, token.getTokenType() + " " + token.getValue());
 		HttpEntity info = new HttpEntity(headers);
 		ResponseEntity<String> response = restTemplate.exchange(authorizationUrl + "/userinfo", HttpMethod.GET, info, String.class);
 		Map<String, Object> responseMap = JsonUtil.convertJsonToMap(response.getBody());
@@ -94,17 +100,26 @@ public class OauthClient {
 		restTemplate.put(authorizationUrl + "/User/{id}/password", httpEntity, userId);
 	}
 
-	private ImplicitResourceDetails getImplicitResource() {
-		ImplicitResourceDetails resource = new ImplicitResourceDetails();
+	private AccessTokenRequest createAccessTokenRequest(String username, String password) {
+		Map<String, String> parameters = new LinkedHashMap<String, String>();
+		parameters.put("credentials", String.format("{\"username\":\"%s\",\"password\":\"%s\"}", username, password));
+		AccessTokenRequest request = new DefaultAccessTokenRequest();
+		request.setAll(parameters);
+		
+		return request;
+	}
+	
+	private OAuth2ProtectedResourceDetails getResourceDetails(String username, String password) {
+		ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+		resource.setUsername(username);
+		resource.setPassword(password);
+
 		String clientId = "cf";
 		resource.setClientId(clientId);
 		resource.setId(clientId);
 		resource.setClientAuthenticationScheme(AuthenticationScheme.header);
-		resource.setAccessTokenUri(authorizationUrl + "/oauth/authorize");
-		String redirectUri = "https://uaa.cloudfoundry.com/redirect/cf";
-		resource.setPreEstablishedRedirectUri(redirectUri);
-		resource.setUseCurrentUri(false);
+		resource.setAccessTokenUri(authorizationUrl + "/oauth/token");
+		
 		return resource;
 	}
-
 }

cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/CloudFoundryClientTest.java
@@ -968,6 +968,18 @@ public class CloudFoundryClientTest {
 		connectedClient.login();
 	}
 
+	@Test
+	@Ignore("This test takes, by design, at least 10 minutes. Enable this only when dealing with authentication issues "
+			+ "or a change in the client side OAuth implementation")
+	public void dealingWithExpiredToken() throws Exception {
+		// The current token expiration time is 10 minutes. If we can still make authenticated calls past that,
+		// then the transparent token refresh scheme working as expected.
+		for (int i = 0; i < 30; i++) {
+			System.out.println("Elapsed time since the last login (at least) " + i/2 + " minutes");
+			getServiceOfferings();
+			Thread.sleep(30 * 1000);			
+		}
+	}
 	
 	@Test
 	public void getRestLog() throws IOException {

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudCredentials.java
@@ -16,6 +16,8 @@
 
 package org.cloudfoundry.client.lib;
 
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+
 /**
  * Class that encapsulates credentials used for authentication
  *
@@ -27,7 +29,7 @@ public class CloudCredentials {
 
 	private String password;
 
-	private String token;
+	private OAuth2AccessToken token;
 
 	private String proxyUser;
 
@@ -47,7 +49,7 @@ public class CloudCredentials {
 	 *
 	 * @param token token to use for authorization
 	 */
-	public CloudCredentials(String token) {
+	public CloudCredentials(OAuth2AccessToken token) {
 		this.token = token;
 	}
 
@@ -87,7 +89,7 @@ public class CloudCredentials {
 	 *
 	 * @return the token
 	 */
-	public String getToken() {
+	public OAuth2AccessToken getToken() {
 		return token;
 	}
 

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudFoundryClient.java
@@ -39,6 +39,7 @@ import org.cloudfoundry.client.lib.domain.Staging;
 import org.cloudfoundry.client.lib.rest.CloudControllerClient;
 import org.cloudfoundry.client.lib.rest.CloudControllerClientFactory;
 import org.cloudfoundry.client.lib.util.RestUtil;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.util.Assert;
 
 /**
@@ -134,7 +135,7 @@ public class CloudFoundryClient implements CloudFoundryOperations {
 		cc.unregister();
 	}
 
-	public String login() {
+	public OAuth2AccessToken login() {
 		return cc.login();
 	}
 

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudFoundryOperations.java
@@ -34,6 +34,7 @@ import org.cloudfoundry.client.lib.domain.CloudSpace;
 import org.cloudfoundry.client.lib.domain.CrashesInfo;
 import org.cloudfoundry.client.lib.domain.InstancesInfo;
 import org.cloudfoundry.client.lib.domain.Staging;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 /**
  * The interface defining operations making up the Cloud Foundry Java client's API.
@@ -102,7 +103,7 @@ public interface CloudFoundryOperations {
 	 *
 	 * @return authentication token
 	 */
-	String login();
+	OAuth2AccessToken login();
 
 	/**
 	 * Logout closing the current session.

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/CloudControllerClient.java
@@ -40,6 +40,7 @@ import org.cloudfoundry.client.lib.domain.CloudSpace;
 import org.cloudfoundry.client.lib.domain.CrashesInfo;
 import org.cloudfoundry.client.lib.domain.InstancesInfo;
 import org.cloudfoundry.client.lib.domain.Staging;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 /**
  * Interface defining operations available for the cloud controller REST client implementations
@@ -56,7 +57,7 @@ public interface CloudControllerClient {
 
 	List<CloudSpace> getSpaces();
 
-	String login();
+	OAuth2AccessToken login();
 
 	void logout();
 

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/CloudControllerClientImpl.java
@@ -142,7 +142,7 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 
 	protected URL authorizationEndpoint;
 
-	protected String token;
+	protected OAuth2AccessToken token;
 
 	private final Log logger;
 	
@@ -160,7 +160,7 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 			URL authorizationEndpoint, String orgName, String spaceName, HttpProxyConfiguration httpProxyConfiguration) {
 		logger = LogFactory.getLog(getClass().getName());
 		CloudControllerClientImpl tempClient = new CloudControllerClientImpl(cloudControllerUrl, restUtil, cloudCredentials, 
-				                                                          authorizationEndpoint, null, httpProxyConfiguration);
+				                                                            authorizationEndpoint, null, httpProxyConfiguration);
 		if (tempClient.token == null) {
 			tempClient.login();
 		}
@@ -382,7 +382,6 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 
 	private class CloudFoundryClientHttpRequestFactory implements ClientHttpRequestFactory {
 
-		private static final String LEGACY_TOKEN_PREFIX = "0408";
 		private ClientHttpRequestFactory delegate;
 		private Integer defaultSocketTimeout = 0;
 
@@ -394,10 +393,10 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 		public ClientHttpRequest createRequest(URI uri, HttpMethod httpMethod) throws IOException {
 			ClientHttpRequest request = delegate.createRequest(uri, httpMethod);
 			if (token != null) {
-				String header = token;
-				if (!header.startsWith(LEGACY_TOKEN_PREFIX) && !header.toLowerCase().startsWith("bearer")) {
-					header = "Bearer " + header; // UAA token without OAuth prefix
+				if (token.getExpiresIn() < 50) { // 50 seconds before expiration? Then refresh it.
+					token = oauthClient.refreshToken(token, cloudCredentials.getEmail(),	cloudCredentials.getPassword());
 				}
+				String header = token.getTokenType() + " " + token.getValue();
 				request.getHeaders().add(AUTHORIZATION_HEADER_KEY, header);
 			}
 			if (cloudCredentials != null && cloudCredentials.getProxyUser() != null) {
@@ -669,11 +668,11 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 		return spaces;
 	}
 
-	public String login() {
-		OAuth2AccessToken token = oauthClient.getToken(cloudCredentials.getEmail(),
+	public OAuth2AccessToken login() {
+		token = oauthClient.getToken(cloudCredentials.getEmail(),
 				cloudCredentials.getPassword());
-		this.token = token.getTokenType() + " " + token.getValue();
-		return this.token;
+		
+		return token;
 	}
 
 	public void logout() {
@@ -1646,9 +1645,10 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 		//TODO: remove this temporary hack once the /v2/users/ uri can be accessed by mere mortals
 		String userJson = "{}";
 		if (token != null) {
-			int x = token.indexOf('.');
-			int y = token.indexOf('.', x + 1);
-			String encodedString = token.substring(x + 1, y);
+			String tokenString = token.getValue();
+			int x = tokenString.indexOf('.');
+			int y = tokenString.indexOf('.', x + 1);
+			String encodedString = tokenString.substring(x + 1, y);
 			try {
 				byte[] decodedBytes = new sun.misc.BASE64Decoder().decodeBuffer(encodedString);
 				userJson = new String(decodedBytes, 0, decodedBytes.length, "UTF-8");

commit 8a528f9abbd7e30c287ae93dd3ed1b162c7410e3

Fixed regression introduced in edcb7a8 with does not propagate the HTTP proxy config to OAuth anymore
To test that the CF client is able to go through a proxy, we point the CC client to a broken url (by adding the suffix "injvmproxy.io" to the hostname). This broken url can only be resolved by going through an inJVM proxy which rewrites the URI. This can be disabled by setting the system property http.skipInJvmProxy to true. This inJvm proxy is implemented using jetty8 and its ProxyServlet. Jetty9 seems much improved but requires to upgrade to java7. It also records number of received requests which some tests can assert.

Limitations with this proxy-support testing method:
- the jetty8 ProxyServlet does not support proxy chaining. Therefore when the Junit test does not have direct access to the CC HTTPS API the test breaks with a 403.
- test infra in cloudfoundry-client-lib are lacking a logging framework. As a workaround system out traces were used instead.
- the CC client is following the URI returned in REST payload, that don't have the  "injvmproxy.io" suffix to their hostname (e.g. https://login.run.pivotal.io/oauth/token). Therefore, some requests might be directly issued by the CC client skipping the proxy and still be unnoticed by tests. A possible solution would be to ensure all remote socket openings are only done by the inJvm proxy (i.e. jetty servlet) and never from the main junit thread. This seems possible fairly easily with using byteman: http://blog.javabenchmark.org/2013/04/fault-injection-in-your-junit-with.html

Please comment on this PR that my investment in automatically testing proxy-related regressions is of interest. In which case I would proceed with this and investigate solving the limitations above.

cloudfoundry-client-lib/pom.xml
@@ -20,6 +20,7 @@
 	<properties>
 		<spring.framework.version>3.0.7.RELEASE</spring.framework.version>
 		<spring.security.oauth.version>1.0.0.RELEASE</spring.security.oauth.version>
+        <jetty.version>8.1.12.v20130726</jetty.version>
 	</properties>
 
 	<profiles>
@@ -121,6 +122,24 @@
 			<version>${spring.framework.version}</version>
 			<scope>test</scope>
 		</dependency>
+        <dependency>
+            <groupId>org.eclipse.jetty</groupId>
+            <artifactId>jetty-server</artifactId>
+            <version>${jetty.version}</version>
+            <scope>test</scope>
+        </dependency>
+        <dependency>
+            <groupId>org.eclipse.jetty</groupId>
+            <artifactId>jetty-servlet</artifactId>
+            <version>${jetty.version}</version>
+            <scope>test</scope>
+        </dependency>
+        <dependency>
+            <groupId>org.eclipse.jetty</groupId>
+            <artifactId>jetty-servlets</artifactId>
+            <version>${jetty.version}</version>
+            <scope>test</scope>
+        </dependency>
 		<dependency>
 			<groupId>log4j</groupId>
 			<artifactId>log4j</artifactId>

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/oauth2/OauthClient.java
@@ -62,6 +62,7 @@ public class OauthClient {
 		AccessTokenRequest request = createAccessTokenRequest(username, password);
 		
 		ResourceOwnerPasswordAccessTokenProvider provider = new ResourceOwnerPasswordAccessTokenProvider();
+        provider.setRequestFactory(restTemplate.getRequestFactory()); //copy the http proxy along
 		OAuth2AccessToken token = null;
 		try {
 			token = provider.obtainAccessToken(resource, request);

cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/CloudFoundryClientTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assume.assumeTrue;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,6 +24,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.cloudfoundry.client.lib.domain.ApplicationStats;
 import org.cloudfoundry.client.lib.domain.CloudApplication;
@@ -42,6 +44,10 @@ import org.cloudfoundry.client.lib.domain.InstanceStats;
 import org.cloudfoundry.client.lib.domain.InstancesInfo;
 import org.cloudfoundry.client.lib.domain.Staging;
 import org.cloudfoundry.client.lib.util.RestUtil;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -70,7 +76,8 @@ import org.springframework.web.client.RestTemplate;
  */
 public class CloudFoundryClientTest {
 
-	private CloudFoundryClient connectedClient;
+    private static final String FAKE_FDQN_PROXIED_SUFFIX = ".injvmproxy.io";
+    private CloudFoundryClient connectedClient;
 
 	// Pass -Dccng.target=http://api.cloudfoundry.com, vcap.me, or your own cloud -- must point to a v2 cloud controller
 	private static final String CCNG_API_URL = System.getProperty("ccng.target", "http://ccng.cloudfoundry.com");
@@ -92,9 +99,15 @@ public class CloudFoundryClientTest {
 	private static final  String TEST_DOMAIN = System.getProperty("vcap.test.domain", defaultNamespace(CCNG_USER_EMAIL) + ".com");
 	
 	private static final boolean SILENT_TEST_TIMINGS = Boolean.getBoolean("silent.testTimings");
-	
-	private static String defaultDomainName = null;
+
+    private static final boolean SKIP_INJVM_PROXY = Boolean.getBoolean("http.skipInJvmProxy");
+
+
+    private static String defaultDomainName = null;
     private HttpProxyConfiguration httpProxyConfiguration;
+    private Server inJvmProxyServer;
+    private int inJvmProxyPort;
+    private AtomicInteger nbInJvmProxyRcvReqs;
 
     private static final String SERVICE_TEST_MYSQL_PLAN = "spark";
     private static final int DEFAULT_MEMORY = 512; // MB
@@ -125,25 +138,33 @@ public class CloudFoundryClientTest {
 				System.out.println("Test " + description.getMethodName() + " took " + (System.currentTimeMillis() - startTime) + " ms");
 			}
 		}
-	};	
+	};
 
-	@BeforeClass
+    @BeforeClass
 	public static void printTargetCloudInfo() {
 		System.out.println("Running tests on " + CCNG_API_URL + " on behalf of " + CCNG_USER_EMAIL);
 		System.out.println("Using space " + CCNG_USER_SPACE + " of organization " + CCNG_USER_ORG );
 		if (CCNG_USER_PASS == null) {
 			fail("System property ccng.passwd must be specified, supply -Dccng.passwd=<password>");
 		}
-
 	}
 
 	@Before
-	public void setUp() throws MalformedURLException {
+	public void setUp() throws Exception {
         if (CCNG_API_PROXY_HOST != null) {
             httpProxyConfiguration = new HttpProxyConfiguration(CCNG_API_PROXY_HOST, CCNG_API_PROXY_PORT);
         }
-        connectedClient = new CloudFoundryClient(new CloudCredentials(CCNG_USER_EMAIL, CCNG_USER_PASS), 
-												new URL(CCNG_API_URL), CCNG_USER_ORG, CCNG_USER_SPACE, httpProxyConfiguration);
+        URL cloudControllerUrl;
+        if (! SKIP_INJVM_PROXY) {
+            startInJvmProxy();
+            HttpProxyConfiguration inJvmHttpProxyConfiguration = new HttpProxyConfiguration("127.0.0.1", inJvmProxyPort);
+            httpProxyConfiguration = inJvmHttpProxyConfiguration;
+            cloudControllerUrl = new URL(CCNG_API_URL + FAKE_FDQN_PROXIED_SUFFIX);
+        } else {
+            cloudControllerUrl = new URL(CCNG_API_URL);
+        }
+        connectedClient = new CloudFoundryClient(new CloudCredentials(CCNG_USER_EMAIL, CCNG_USER_PASS),
+                cloudControllerUrl, CCNG_USER_ORG, CCNG_USER_SPACE, httpProxyConfiguration);
 		connectedClient.login();
 		defaultDomainName = getDefaultDomain(connectedClient.getDomainsForOrg()).getName();
 		
@@ -158,12 +179,15 @@ public class CloudFoundryClientTest {
 	}
 
 	@After
-	public void tearDown() {
+	public void tearDown() throws Exception {
 		// Clean after ourselves so that there are no leftover apps, services, domains, and routes
 		connectedClient.deleteAllApplications();
 		connectedClient.deleteAllServices();
 		clearTestDomainAndRoutes();
 		tearDownComplete = true;
+
+        inJvmProxyServer.stop();
+        nbInJvmProxyRcvReqs.set(0);
 	}
 
 	@Test
@@ -172,9 +196,42 @@ public class CloudFoundryClientTest {
 		assertNotNull(info.getName());
 		assertNotNull(info.getSupport());
 		assertNotNull(info.getBuild());
-	}
+        assertTrue(SKIP_INJVM_PROXY || nbInJvmProxyRcvReqs.get() >1 );
+    }
 
-	@Test
+    private int getNextAvailablePort(int initial) {
+        int current = initial;
+        while (! PortAvailability.available(current)) {
+            current ++;
+            if (current - initial > 100) {
+                throw new RuntimeException("did not find an available port from " + initial + " up to:" + current);
+            }
+        }
+        return current;
+    }
+
+    /**
+     * To test that the CF client is able to go through a proxy, we point the CC client to a broken url
+     * that can only be resolved by going through an inJVM proxy which rewrites the URI.
+     * This method starts this inJvm proxy.
+     * @throws Exception
+     */
+    private void startInJvmProxy() throws Exception {
+        inJvmProxyPort = getNextAvailablePort(8080);
+        inJvmProxyServer = new Server(inJvmProxyPort);
+        QueuedThreadPool threadPool = new QueuedThreadPool();
+        threadPool.setMinThreads(1);
+        inJvmProxyServer.setThreadPool(threadPool);
+        ServletHandler handler = new ServletHandler();
+        inJvmProxyServer.setHandler(handler);
+        nbInJvmProxyRcvReqs = new AtomicInteger();
+        InterceptingProxyServlet interceptingProxyServlet = new InterceptingProxyServlet(httpProxyConfiguration, FAKE_FDQN_PROXIED_SUFFIX, nbInJvmProxyRcvReqs);
+        handler.addServletWithMapping(new ServletHolder(interceptingProxyServlet), "/*");
+        inJvmProxyServer.start();
+    }
+
+
+    @Test
 	public void spacesAvailable() throws Exception {
 		List<CloudSpace> spaces = connectedClient.getSpaces();
 		assertNotNull(spaces);

cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/InterceptingProxyServlet.java
@@ -1,0 +1,114 @@
+package org.cloudfoundry.client.lib;
+
+import org.eclipse.jetty.client.Address;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpExchange;
+import org.eclipse.jetty.continuation.Continuation;
+import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.servlets.ProxyServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * An http proxy implementation that corrects fake host name to ensure some request indeed go through the proxy
+ * as expected
+ */
+public class InterceptingProxyServlet extends ProxyServlet {
+    private AtomicInteger nbReceivedRequests;
+    /**
+     * If ever this junit test needs a proxy to reach the Cf instance, we direct to it.
+     */
+    private HttpProxyConfiguration httpProxyConfiguration;
+    private String fakeFdqnProxiedSuffix;
+
+    public InterceptingProxyServlet(HttpProxyConfiguration httpProxyConfiguration, String fakeFdqnProxiedSuffix, AtomicInteger nbReceivedRequests) {
+        this.httpProxyConfiguration = httpProxyConfiguration;
+        this.fakeFdqnProxiedSuffix = fakeFdqnProxiedSuffix;
+        this.nbReceivedRequests = nbReceivedRequests;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        final Request request = (Request) req;
+
+        //Setting the host header in the proxy servlet is not sufficient, we do also need to rewrite the uri and the servername.
+
+        String host = request.getServerName();
+        if (host.endsWith(fakeFdqnProxiedSuffix)) {
+            host = host.substring(0, host.length() - fakeFdqnProxiedSuffix.length());
+            setHostHeader(host);
+            request.setServerName(host);
+        }
+
+        String requestURI = request.getUri().toString();
+        int fakeIndex = requestURI.indexOf(fakeFdqnProxiedSuffix);
+        if (fakeIndex >0) {
+            requestURI = removeFakeSuffixFromUri(requestURI, fakeIndex);
+            request.setUri(new HttpURI(requestURI));
+        }
+
+        nbReceivedRequests.incrementAndGet();
+
+        super.service(req, res);
+    }
+
+    /**
+    @Override
+    protected HttpURI proxyHttpURI(final String scheme, String serverName, int serverPort, String uri) throws MalformedURLException {
+        int fakeIndex = serverName.indexOf(fakeFdqnProxiedSuffix);
+        if (fakeIndex >0) {
+            serverName = removeFakeSuffixFromUri(serverName, fakeIndex);
+        }
+        return super.proxyHttpURI(scheme, serverName, serverPort, uri);
+    }
+
+    */
+
+    /**
+    @Override
+    protected void customizeExchange(HttpExchange exchange, HttpServletRequest request) {
+        String requestURI = exchange.getRequestURI();
+        int fakeIndex = requestURI.indexOf(fakeFdqnProxiedSuffix);
+        if (fakeIndex >0) {
+            requestURI = removeFakeSuffixFromUri(requestURI, fakeIndex);
+            exchange.setRequestURI(requestURI);
+        }
+    }
+
+    */
+    private String removeFakeSuffixFromUri(String requestURI, int fakeIndex) {
+        requestURI = requestURI.substring(0, fakeIndex) + requestURI.substring(fakeIndex+fakeFdqnProxiedSuffix.length());
+        return requestURI;
+    }
+
+    @Override
+    protected HttpClient createHttpClientInstance() {
+        HttpClient httpClient = super.createHttpClientInstance();
+        if (httpProxyConfiguration != null) {
+            httpClient.setProxy(new Address(httpProxyConfiguration.getProxyHost(), httpProxyConfiguration.getProxyPort()));
+        }
+        return httpClient;
+    }
+
+    /** jetty 9 impl variant which imposes upgrade to java 7
+
+
+    @Override
+    protected HttpClient createHttpClient() throws ServletException {
+        HttpClient httpClient = super.createHttpClient();
+        if (httpProxyConfiguration != null) {
+            ProxyConfiguration proxyConfiguration = new ProxyConfiguration(httpProxyConfiguration.getProxyHost(), httpProxyConfiguration.getProxyPort());
+            httpClient.setProxyConfiguration(proxyConfiguration);
+        }
+        return httpClient;
+    }
+     */
+}

cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/PortAvailability.java
@@ -1,0 +1,77 @@
+package org.cloudfoundry.client.lib;
+
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * Utility class used within the InJvm http proxy tests
+ * copied from https://github.com/TheLadders/embedded-test-jetty */
+public final class PortAvailability
+{
+  private static final Logger LOGGER = Logger.getLogger(PortAvailability.class.getName());
+
+
+  /**
+   * Checks to see if a specific port is available.
+   * 
+   * @param port
+   *          the port to check for availability
+   */
+  /*
+   * Slightly adapted from Apache MINA (took out min/max port check)
+   */
+  public static boolean available(int port)
+  {
+    ServerSocket ss = null;
+    DatagramSocket ds = null;
+    try
+    {
+      ss = new ServerSocket(port);
+      ds = new DatagramSocket(port);
+      ss.setReuseAddress(true);
+      ds.setReuseAddress(true);
+      return true;
+    }
+    catch (IOException e)
+    {
+      LOGGER.log(Level.FINE, "Error checking port availability", e);
+    }
+    finally
+    {
+      close(ss);
+      close(ds);
+    }
+
+    return false;
+  }
+
+
+  private static void close(DatagramSocket ds)
+  {
+    if (ds != null)
+    {
+      ds.close();
+    }
+  }
+
+
+  private static void close(ServerSocket ss)
+  {
+    if (ss != null)
+    {
+      try
+      {
+        ss.close();
+      }
+      catch (IOException e)
+      {
+        LOGGER.log(Level.WARNING, "Error closing socket", e);
+      }
+    }
+  }
+}