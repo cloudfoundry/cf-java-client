cloudfoundry/cf-java-client

commit edcb7a845f5ccfd90dbad21bef9f6b0fff6cffbf

Make client deal with expired token transparently.
- Use OwnerPassword resource instead of implicit to match how it is done is cf
- Check if the token is about to expire and use refresh token if so.
- Remove unneeded unit tests

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/oauth2/OauthClient.java
@@ -32,8 +32,8 @@ import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedExc
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.AccessTokenRequest;
 import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
-import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
-import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.security.oauth2.common.AuthenticationScheme;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.client.RestTemplate;
@@ -58,13 +58,10 @@ public class OauthClient {
 	}
 
 	public OAuth2AccessToken getToken(String username, String password) {
-		OAuth2ProtectedResourceDetails resource = getImplicitResource();
-		Map<String, String> parameters = new LinkedHashMap<String, String>();
-		parameters.put("credentials", String.format("{\"username\":\"%s\",\"password\":\"%s\"}", username, password));
-		AccessTokenRequest request = new DefaultAccessTokenRequest();
-		request.setAll(parameters);
-		ImplicitAccessTokenProvider provider = new ImplicitAccessTokenProvider();
-		provider.setRestTemplate(restTemplate);
+		OAuth2ProtectedResourceDetails resource = getResourceDetails(username, password);
+		AccessTokenRequest request = createAccessTokenRequest(username, password);
+		
+		ResourceOwnerPasswordAccessTokenProvider provider = new ResourceOwnerPasswordAccessTokenProvider();
 		OAuth2AccessToken token = null;
 		try {
 			token = provider.obtainAccessToken(resource, request);
@@ -77,11 +74,20 @@ public class OauthClient {
 		}
 		return token;
 	}
+	
+	public OAuth2AccessToken refreshToken(OAuth2AccessToken currentToken, String username, String password) {
+		OAuth2ProtectedResourceDetails resource = getResourceDetails(username, password);
+		AccessTokenRequest request = createAccessTokenRequest(username, password);
+
+		ResourceOwnerPasswordAccessTokenProvider provider = new ResourceOwnerPasswordAccessTokenProvider();
+		
+		return provider.refreshAccessToken(resource, currentToken.getRefreshToken(), request);
+	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void changePassword(String token, String oldPassword, String newPassword) {
+	public void changePassword(OAuth2AccessToken token, String oldPassword, String newPassword) {
 		HttpHeaders headers = new HttpHeaders();
-		headers.add(AUTHORIZATION_HEADER_KEY, token);
+		headers.add(AUTHORIZATION_HEADER_KEY, token.getTokenType() + " " + token.getValue());
 		HttpEntity info = new HttpEntity(headers);
 		ResponseEntity<String> response = restTemplate.exchange(authorizationUrl + "/userinfo", HttpMethod.GET, info, String.class);
 		Map<String, Object> responseMap = JsonUtil.convertJsonToMap(response.getBody());
@@ -94,17 +100,26 @@ public class OauthClient {
 		restTemplate.put(authorizationUrl + "/User/{id}/password", httpEntity, userId);
 	}
 
-	private ImplicitResourceDetails getImplicitResource() {
-		ImplicitResourceDetails resource = new ImplicitResourceDetails();
+	private AccessTokenRequest createAccessTokenRequest(String username, String password) {
+		Map<String, String> parameters = new LinkedHashMap<String, String>();
+		parameters.put("credentials", String.format("{\"username\":\"%s\",\"password\":\"%s\"}", username, password));
+		AccessTokenRequest request = new DefaultAccessTokenRequest();
+		request.setAll(parameters);
+		
+		return request;
+	}
+	
+	private OAuth2ProtectedResourceDetails getResourceDetails(String username, String password) {
+		ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+		resource.setUsername(username);
+		resource.setPassword(password);
+
 		String clientId = "cf";
 		resource.setClientId(clientId);
 		resource.setId(clientId);
 		resource.setClientAuthenticationScheme(AuthenticationScheme.header);
-		resource.setAccessTokenUri(authorizationUrl + "/oauth/authorize");
-		String redirectUri = "https://uaa.cloudfoundry.com/redirect/cf";
-		resource.setPreEstablishedRedirectUri(redirectUri);
-		resource.setUseCurrentUri(false);
+		resource.setAccessTokenUri(authorizationUrl + "/oauth/token");
+		
 		return resource;
 	}
-
 }

cloudfoundry-client-lib/pom.xml
@@ -19,7 +19,7 @@
 
 	<properties>
 		<spring.framework.version>3.0.7.RELEASE</spring.framework.version>
-		<spring.security.oauth.version>1.0.0.RC1</spring.security.oauth.version>
+		<spring.security.oauth.version>1.0.0.RELEASE</spring.security.oauth.version>
 	</properties>
 
 	<profiles>

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudCredentials.java
@@ -16,6 +16,8 @@
 
 package org.cloudfoundry.client.lib;
 
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+
 /**
  * Class that encapsulates credentials used for authentication
  *
@@ -27,7 +29,7 @@ public class CloudCredentials {
 
 	private String password;
 
-	private String token;
+	private OAuth2AccessToken token;
 
 	private String proxyUser;
 
@@ -47,7 +49,7 @@ public class CloudCredentials {
 	 *
 	 * @param token token to use for authorization
 	 */
-	public CloudCredentials(String token) {
+	public CloudCredentials(OAuth2AccessToken token) {
 		this.token = token;
 	}
 
@@ -87,7 +89,7 @@ public class CloudCredentials {
 	 *
 	 * @return the token
 	 */
-	public String getToken() {
+	public OAuth2AccessToken getToken() {
 		return token;
 	}
 

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudFoundryClient.java
@@ -39,6 +39,7 @@ import org.cloudfoundry.client.lib.domain.Staging;
 import org.cloudfoundry.client.lib.rest.CloudControllerClient;
 import org.cloudfoundry.client.lib.rest.CloudControllerClientFactory;
 import org.cloudfoundry.client.lib.util.RestUtil;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.util.Assert;
 
 /**
@@ -134,7 +135,7 @@ public class CloudFoundryClient implements CloudFoundryOperations {
 		cc.unregister();
 	}
 
-	public String login() {
+	public OAuth2AccessToken login() {
 		return cc.login();
 	}
 

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudFoundryOperations.java
@@ -34,6 +34,7 @@ import org.cloudfoundry.client.lib.domain.CloudSpace;
 import org.cloudfoundry.client.lib.domain.CrashesInfo;
 import org.cloudfoundry.client.lib.domain.InstancesInfo;
 import org.cloudfoundry.client.lib.domain.Staging;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 /**
  * The interface defining operations making up the Cloud Foundry Java client's API.
@@ -102,7 +103,7 @@ public interface CloudFoundryOperations {
 	 *
 	 * @return authentication token
 	 */
-	String login();
+	OAuth2AccessToken login();
 
 	/**
 	 * Logout closing the current session.

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/CloudControllerClient.java
@@ -40,6 +40,7 @@ import org.cloudfoundry.client.lib.domain.CloudSpace;
 import org.cloudfoundry.client.lib.domain.CrashesInfo;
 import org.cloudfoundry.client.lib.domain.InstancesInfo;
 import org.cloudfoundry.client.lib.domain.Staging;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 /**
  * Interface defining operations available for the cloud controller REST client implementations
@@ -56,7 +57,7 @@ public interface CloudControllerClient {
 
 	List<CloudSpace> getSpaces();
 
-	String login();
+	OAuth2AccessToken login();
 
 	void logout();
 

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/CloudControllerClientImpl.java
@@ -142,7 +142,7 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 
 	protected URL authorizationEndpoint;
 
-	protected String token;
+	protected OAuth2AccessToken token;
 
 	private final Log logger;
 	
@@ -160,7 +160,7 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 			URL authorizationEndpoint, String orgName, String spaceName, HttpProxyConfiguration httpProxyConfiguration) {
 		logger = LogFactory.getLog(getClass().getName());
 		CloudControllerClientImpl tempClient = new CloudControllerClientImpl(cloudControllerUrl, restUtil, cloudCredentials, 
-				                                                          authorizationEndpoint, null, httpProxyConfiguration);
+				                                                            authorizationEndpoint, null, httpProxyConfiguration);
 		if (tempClient.token == null) {
 			tempClient.login();
 		}
@@ -382,7 +382,6 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 
 	private class CloudFoundryClientHttpRequestFactory implements ClientHttpRequestFactory {
 
-		private static final String LEGACY_TOKEN_PREFIX = "0408";
 		private ClientHttpRequestFactory delegate;
 		private Integer defaultSocketTimeout = 0;
 
@@ -394,10 +393,10 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 		public ClientHttpRequest createRequest(URI uri, HttpMethod httpMethod) throws IOException {
 			ClientHttpRequest request = delegate.createRequest(uri, httpMethod);
 			if (token != null) {
-				String header = token;
-				if (!header.startsWith(LEGACY_TOKEN_PREFIX) && !header.toLowerCase().startsWith("bearer")) {
-					header = "Bearer " + header; // UAA token without OAuth prefix
+				if (token.getExpiresIn() < 50) { // 50 seconds before expiration? Then refresh it.
+					token = oauthClient.refreshToken(token, cloudCredentials.getEmail(),	cloudCredentials.getPassword());
 				}
+				String header = token.getTokenType() + " " + token.getValue();
 				request.getHeaders().add(AUTHORIZATION_HEADER_KEY, header);
 			}
 			if (cloudCredentials != null && cloudCredentials.getProxyUser() != null) {
@@ -669,11 +668,11 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 		return spaces;
 	}
 
-	public String login() {
-		OAuth2AccessToken token = oauthClient.getToken(cloudCredentials.getEmail(),
+	public OAuth2AccessToken login() {
+		token = oauthClient.getToken(cloudCredentials.getEmail(),
 				cloudCredentials.getPassword());
-		this.token = token.getTokenType() + " " + token.getValue();
-		return this.token;
+		
+		return token;
 	}
 
 	public void logout() {
@@ -1646,9 +1645,10 @@ public class CloudControllerClientImpl implements CloudControllerClient {
 		//TODO: remove this temporary hack once the /v2/users/ uri can be accessed by mere mortals
 		String userJson = "{}";
 		if (token != null) {
-			int x = token.indexOf('.');
-			int y = token.indexOf('.', x + 1);
-			String encodedString = token.substring(x + 1, y);
+			String tokenString = token.getValue();
+			int x = tokenString.indexOf('.');
+			int y = tokenString.indexOf('.', x + 1);
+			String encodedString = tokenString.substring(x + 1, y);
 			try {
 				byte[] decodedBytes = new sun.misc.BASE64Decoder().decodeBuffer(encodedString);
 				userJson = new String(decodedBytes, 0, decodedBytes.length, "UTF-8");

cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/CloudFoundryClientTest.java
@@ -968,6 +968,18 @@ public class CloudFoundryClientTest {
 		connectedClient.login();
 	}
 
+	@Test
+	@Ignore("This test takes, by design, at least 10 minutes. Enable this only when dealing with authentication issues "
+			+ "or a change in the client side OAuth implementation")
+	public void dealingWithExpiredToken() throws Exception {
+		// The current token expiration time is 10 minutes. If we can still make authenticated calls past that,
+		// then the transparent token refresh scheme working as expected.
+		for (int i = 0; i < 30; i++) {
+			System.out.println("Elapsed time since the last login (at least) " + i/2 + " minutes");
+			getServiceOfferings();
+			Thread.sleep(30 * 1000);			
+		}
+	}
 	
 	@Test
 	public void getRestLog() throws IOException {

commit 1797f01f064869850d8dac3755b2cc403986136a

Additional fix for regression introduced in edcb7a8 with does not propagate the HTTP proxy config to OAuth anymore
Propagating fix to refreshToken() which was also impacted. Moved the propagation of the request factory into the createResourceOwnerPasswordAccessTokenProvider() method.  It was hard/verbose to unit test this with little added value. I'll rather complete the integration test which is still on my todo list.

cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/oauth2/OauthClient.java
@@ -41,8 +41,8 @@ import org.springframework.web.client.RestTemplate;
 /**
  * Client that can handle authentication against a UAA instance
  *
- * @author: Dave Syer
- * @author: Thomas Risberg
+ * @author Dave Syer
+ * @author Thomas Risberg
  */
 public class OauthClient {
 
@@ -62,8 +62,7 @@ public class OauthClient {
 		AccessTokenRequest request = createAccessTokenRequest(username, password);
 
         ResourceOwnerPasswordAccessTokenProvider provider = createResourceOwnerPasswordAccessTokenProvider();
-        provider.setRequestFactory(restTemplate.getRequestFactory()); //copy the http proxy along
-        OAuth2AccessToken token = null;
+        OAuth2AccessToken token;
 		try {
 			token = provider.obtainAccessToken(resource, request);
 		}
@@ -77,7 +76,9 @@ public class OauthClient {
 	}
 
     protected ResourceOwnerPasswordAccessTokenProvider createResourceOwnerPasswordAccessTokenProvider() {
-        return new ResourceOwnerPasswordAccessTokenProvider();
+        ResourceOwnerPasswordAccessTokenProvider resourceOwnerPasswordAccessTokenProvider = new ResourceOwnerPasswordAccessTokenProvider();
+        resourceOwnerPasswordAccessTokenProvider.setRequestFactory(restTemplate.getRequestFactory()); //copy the http proxy along
+        return resourceOwnerPasswordAccessTokenProvider;
     }
 
     public OAuth2AccessToken refreshToken(OAuth2AccessToken currentToken, String username, String password) {