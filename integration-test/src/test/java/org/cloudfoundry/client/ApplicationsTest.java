4cb52d0fb088b77f7a798d254859279562e9581c
Ben Hale <bhale@pivotal.io>
2015-10-20 11:35:14 -0700

    First Integration Test
    This change adds the first "real" integration test to the project.  At
    the moment the test in this change does not pass.  This is to be used
    as a way to create the rest of the integration test infrastructure.
    When this test passes, the initial task of integration testing will be
    complete and ready for more tests to be written.

---
+++ integration-test/src/test/java/org/cloudfoundry/client/ApplicationsTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.client;
+
+import org.cloudfoundry.client.v3.PaginatedResponse;
+import org.cloudfoundry.client.v3.applications.CreateApplicationRequest;
+import org.cloudfoundry.client.v3.applications.ListApplicationsRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import reactor.rx.Streams;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ClientConfiguration.class)
+public final class ApplicationsTest {
+
+    @Autowired
+    private volatile CloudFoundryClient cloudFoundryClient;
+
+    @Autowired
+    private volatile String spaceId;
+
+    @Before
+    public void zeroExistingApplications() {
+        ListApplicationsRequest request = new ListApplicationsRequest().withSpaceId(this.spaceId);
+
+        int size = Streams.wrap(this.cloudFoundryClient.applications().list(request))
+                .map(PaginatedResponse::getResources)
+                .map(Collection::size)
+                .next().poll();
+
+        assertEquals("Unexpected applications exist", 0, size);
+    }
+
+    @Test
+    public void create() {
+        CreateApplicationRequest createRequest = new CreateApplicationRequest()
+                .withSpaceId(this.spaceId)
+                .withName("test-name");
+        Streams.wrap(this.cloudFoundryClient.applications().create(createRequest))
+                .next().poll();
+
+        ListApplicationsRequest listRequest = new ListApplicationsRequest().withSpaceId(this.spaceId);
+        int size = Streams.wrap(this.cloudFoundryClient.applications().list(listRequest))
+                .map(PaginatedResponse::getResources)
+                .map(Collection::size)
+                .next().poll();
+
+        assertEquals(1, size);
+    }
+
+}
---
+++ integration-test/src/test/java/org/cloudfoundry/client/ClientConfiguration.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.client;
+
+import org.cloudfoundry.client.spring.SpringCloudFoundryClient;
+import org.cloudfoundry.client.spring.SpringCloudFoundryClientBuilder;
+import org.cloudfoundry.client.spring.SpringLoggregatorClient;
+import org.cloudfoundry.client.spring.SpringLoggregatorClientBuilder;
+import org.cloudfoundry.client.v2.spaces.ListSpacesRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.rx.Streams;
+
+@Configuration
+@EnableAutoConfiguration
+class ClientConfiguration {
+
+    @Bean
+    SpringCloudFoundryClient cloudFoundryClient(@Value("${test.host}") String host,
+                                                @Value("${test.username}") String username,
+                                                @Value("${test.password}") String password,
+                                                @Value("${test.skipSslValidation:false}") Boolean skipSslValidation) {
+        return new SpringCloudFoundryClientBuilder()
+                .withApi(host)
+                .withCredentials(username, password)
+                .withSkipSslValidation(skipSslValidation)
+                .build();
+    }
+
+    @Bean
+    SpringLoggregatorClient loggregatorClient(SpringCloudFoundryClient cloudFoundryClient) {
+        return new SpringLoggregatorClientBuilder()
+                .withCloudFoundryClient(cloudFoundryClient)
+                .build();
+    }
+
+    @Bean
+    String spaceId(@Value("${test.space}") String space, CloudFoundryClient cloudFoundryClient) {
+        return Streams.wrap(cloudFoundryClient.spaces().list(new ListSpacesRequest().withName(space)))
+                .flatMap(response -> Streams.from(response.getResources()))
+                .map(resource -> resource.getMetadata().getId())
+                .next().poll();
+    }
+
+}
