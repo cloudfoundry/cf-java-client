e16e6d57dc3b9565fe20eb451c114aee2ef86e48
Ben Hale <bhale@pivotal.io>
2015-12-11 11:33:31 -0800

    Copyright Updates
    This change updates the copyright of all of the files in the project.

--- cloudfoundry-client/src/test/java/org/cloudfoundry/client/v2/organizations/ListOrganizationSpacesRequestTest.java
+++ cloudfoundry-client/src/test/java/org/cloudfoundry/client/v2/organizations/ListOrganizationSpacesRequestTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2013-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,6 @@ public final class ListOrganizationSpacesRequestTest {
 
         assertEquals(INVALID, result.getStatus());
         assertEquals("id must be specified", result.getMessages().get(0));
-    }        
+    }
 
 }
