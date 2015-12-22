7882fb843499ff75d44afd0d34e03a21a459791e
Christopher Frost <cfrost@pivotal.io>
2015-12-22 10:39:32 +0000

    Associate Manager with the Organization by Username (v2)
    This change adds the v2 API for Associate Manager with the Organization
    by Username (PUT v2/organizations/:guid/managers).
    
    [#110451568]

---
+++ cloudfoundry-client/src/main/lombok/org/cloudfoundry/client/v2/organizations/AssociateOrganizationManagerByUsernameRequest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.client.v2.organizations;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import org.cloudfoundry.client.Validatable;
+import org.cloudfoundry.client.ValidationResult;
+
+/**
+ * The request payload for the Associate Manager with the Organization by Username operation
+ */
+@Data
+public final class AssociateOrganizationManagerByUsernameRequest implements Validatable {
+
+    /**
+     * The id
+     *
+     * @param id the id
+     * @return the id
+     */
+    @Getter(onMethod = @__(@JsonIgnore))
+    private final String id;
+
+    /**
+     * The username
+     *
+     * @param username the username
+     * @return the username
+     */
+    @Getter(onMethod = @__(@JsonProperty("username")))
+    private final String username;
+
+    @Builder
+    AssociateOrganizationManagerByUsernameRequest(String id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    @Override
+    public ValidationResult isValid() {
+        ValidationResult.ValidationResultBuilder builder = ValidationResult.builder();
+
+        if (this.id == null) {
+            builder.message("id must be specified");
+        }
+
+        if (this.username == null) {
+            builder.message("username must be specified");
+        }
+
+        return builder.build();
+    }
+
+}
