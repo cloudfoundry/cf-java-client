ab70e63c08a69d9acaa3154c0deb0c1ff0a71c7e
Ben Hale <bhale@pivotal.io>
2016-01-06 06:30:40 -0800

    Copyright Updates
    This change updates the copyright statement at the top of each file.

--- cloudfoundry-client/src/main/lombok/org/cloudfoundry/client/v2/serviceinstances/ServiceBindingEntity.java
+++ cloudfoundry-client/src/main/lombok/org/cloudfoundry/client/v2/serviceinstances/ServiceBindingEntity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 the original author or authors.
+ * Copyright 2013-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,7 +46,7 @@ public final class ServiceBindingEntity {
     private final String serviceInstanceUrl;
 
     private final String syslogDrainUrl;
-    
+
     @Builder
     ServiceBindingEntity(@JsonProperty("app_guid") String applicationId,
                          @JsonProperty("app_url") String applicationUrl,
