68a55e0a4b67b71e9af65bc7240f6d400240f49a
Glyn Normington <gnormington@pivotal.io>
2016-01-15 14:54:41 +0000

    Integration test client organizations
    [#110817818]

---
+++ integration-test/src/test/java/org/cloudfoundry/client/OrganizationsTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.client;
+
+import org.cloudfoundry.AbstractIntegrationTest;
+import org.cloudfoundry.client.v2.organizations.CreateOrganizationRequest;
+import org.cloudfoundry.client.v2.organizations.DeleteOrganizationRequest;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.fn.tuple.Tuple;
+
+public final class OrganizationsTest extends AbstractIntegrationTest {
+
+    @Test
+    public void create() {
+        CreateOrganizationRequest createOrganizationRequest = CreateOrganizationRequest.builder()
+                .name("test-org")
+                .build();
+
+        this.cloudFoundryClient.organizations().create(createOrganizationRequest)
+                .map(response -> Tuple.of(response.getEntity().getName(),response.getMetadata().getId()))
+                .doOnSuccess(tuple -> deleteOrg(this.cloudFoundryClient, tuple.t2)) // avoid polluting test environment
+                .map(tuple -> tuple.t1)
+                .subscribe(this.testSubscriber().assertEquals("test-org"));
+    }
+
+    private static final void deleteOrg(CloudFoundryClient client, String orgId) {
+        DeleteOrganizationRequest request = DeleteOrganizationRequest.builder()
+                .id(orgId)
+                .build();
+
+        client.organizations().delete(request).get();
+    }
+
+}
