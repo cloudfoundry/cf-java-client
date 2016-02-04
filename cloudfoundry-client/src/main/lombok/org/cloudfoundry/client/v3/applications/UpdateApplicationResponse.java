e50511442f283f4ecb20bd0660e0eeafb642d842
Ben Hale <bhale@pivotal.io>
2016-02-04 13:43:57 -0800

    V3 Application Update
    This change updates the V3 Application type to the new design.
    
    [#113099925]

--- cloudfoundry-client/src/main/lombok/org/cloudfoundry/client/v3/applications/UpdateApplicationResponse.java
+++ cloudfoundry-client/src/main/lombok/org/cloudfoundry/client/v3/applications/UpdateApplicationResponse.java
@@ -22,6 +22,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Singular;
 import lombok.ToString;
+import org.cloudfoundry.client.v3.Lifecycle;
 import org.cloudfoundry.client.v3.Link;
 
 import java.util.Map;
@@ -35,16 +36,16 @@ import java.util.Map;
 public final class UpdateApplicationResponse extends Application {
 
     @Builder
-    UpdateApplicationResponse(@JsonProperty("buildpack") String buildpack,
-                              @JsonProperty("created_at") String createdAt,
+    UpdateApplicationResponse(@JsonProperty("created_at") String createdAt,
                               @JsonProperty("desired_state") String desiredState,
                               @JsonProperty("environment_variables") @Singular Map<String, String> environmentVariables,
                               @JsonProperty("guid") String id,
-                              @JsonProperty("_links") @Singular Map<String, Link> links,
+                              @JsonProperty("lifecycle") Lifecycle lifecycle,
+                              @JsonProperty("links") @Singular Map<String, Link> links,
                               @JsonProperty("name") String name,
                               @JsonProperty("total_desired_instances") Integer totalDesiredInstances,
                               @JsonProperty("updated_at") String updatedAt) {
-        super(buildpack, createdAt, desiredState, environmentVariables, id, links, name, totalDesiredInstances, updatedAt);
+        super(createdAt, desiredState, environmentVariables, id, lifecycle, links, name, totalDesiredInstances, updatedAt);
     }
 
 }
