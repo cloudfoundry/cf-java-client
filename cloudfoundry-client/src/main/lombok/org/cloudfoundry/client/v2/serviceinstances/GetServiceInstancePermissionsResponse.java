d843a0fd00b545d82cd863eec49f0dc9f0476e88
Ben Einaudi <benjamin.einaudi@orange.com>
2016-02-09 12:17:11 +0100

    Get permissions on a Service Instance This change adds the api to get the permissions on a service instance (GET /v2/service_instances/:guid/permissions) [#101451548][#386]

---
+++ cloudfoundry-client/src/main/lombok/org/cloudfoundry/client/v2/serviceinstances/GetServiceInstancePermissionsResponse.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.client.v2.serviceinstances;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * The resource response payload for the Get Permissions Response
+ */
+@Data
+@EqualsAndHashCode
+@ToString(callSuper = true)
+public final class GetServiceInstancePermissionsResponse {
+
+    /**
+     * The manage flag
+     *
+     * @param manage the manage flag
+     * @return the manage flag
+     */
+    private final Boolean manage;
+
+    @Builder
+    GetServiceInstancePermissionsResponse(@JsonProperty("manage") Boolean manage) {
+        this.manage = manage;
+    }
+    
+}
