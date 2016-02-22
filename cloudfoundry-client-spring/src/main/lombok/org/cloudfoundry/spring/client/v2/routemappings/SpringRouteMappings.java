12a25c49f9da08bee8b0675839fba2fb8ed15b8f
Ben Einaudi <benjamin.einaudi@orange.com>
2016-02-22 10:42:40 +0100

    Mapping an App and a Route
    This change adds the api to map an app and a route (POST /v2/route_mappings)
    
    [#112357623][resolves #400]

---
+++ cloudfoundry-client-spring/src/main/lombok/org/cloudfoundry/spring/client/v2/routemappings/SpringRouteMappings.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.spring.client.v2.routemappings;
+
+import lombok.ToString;
+import org.cloudfoundry.client.v2.routemappings.CreateRouteMappingRequest;
+import org.cloudfoundry.client.v2.routemappings.CreateRouteMappingResponse;
+import org.cloudfoundry.client.v2.routemappings.RouteMappings;
+import org.cloudfoundry.spring.util.AbstractSpringOperations;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.SchedulerGroup;
+import reactor.fn.Consumer;
+
+import java.net.URI;
+
+/**
+ * The Spring-based implementation of {@link RouteMappings}
+ */
+@ToString(callSuper = true)
+public final class SpringRouteMappings extends AbstractSpringOperations implements RouteMappings {
+
+    /**
+     * Creates an instance
+     *
+     * @param restOperations the {@link RestOperations} to use to communicate with the server
+     * @param root           the root URI of the server.  Typically something like {@code https://api.run.pivotal.io}.
+     * @param schedulerGroup The group to use when making requests
+     */
+    public SpringRouteMappings(RestOperations restOperations, URI root, SchedulerGroup schedulerGroup) {
+        super(restOperations, root, schedulerGroup);
+    }
+
+    @Override
+    public Mono<CreateRouteMappingResponse> create(final CreateRouteMappingRequest request) {
+        return post(request, CreateRouteMappingResponse.class, new Consumer<UriComponentsBuilder>() {
+
+            @Override
+            public void accept(UriComponentsBuilder builder) {
+                builder.pathSegment("v2", "route_mappings");
+            }
+
+        });
+    }
+
+}
---
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/client/v2/routemappings/RouteMappings.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.client.v2.routemappings;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * Main entry point to the Cloud Foundry Route Mappings V2 Client API
+ */
+public interface RouteMappings {
+
+    /**
+     * Makes the <a href="http://apidocs.cloudfoundry.org/229/routes_mapping/mapping_an_app_and_a_route.html">Creating a Route Mapping</a> request
+     *
+     * @param request the Creating a Route Mapping request
+     * @return the response from the Creating a Route Mapping request
+     */
+    Mono<CreateRouteMappingResponse> create(CreateRouteMappingRequest request);
+
+}
