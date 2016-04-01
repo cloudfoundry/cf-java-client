38f07b0b3e39e7a9670ff0302ec1cd58d1a14e58
Paul Harris <pharris@pivotal.io>
2016-04-01 17:12:12 +0100

    Service Operation
    This change adds the ability to get details of a service instance. It
    also refactors the list service operation to return the same object for
    consistency.
    
    [#106155478]

---
+++ integration-test/src/test/java/org/cloudfoundry/operations/ServicesTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.operations;
+
+import org.cloudfoundry.AbstractIntegrationTest;
+import org.cloudfoundry.operations.services.CreateServiceInstanceRequest;
+import org.cloudfoundry.operations.services.GetServiceInstanceRequest;
+import org.cloudfoundry.operations.services.ServiceInstance;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public final class ServicesTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private CloudFoundryOperations cloudFoundryOperations;
+
+    @Ignore("Needs marketplace feature to derive planName and serviceName - https://www.pivotaltracker.com/story/show/106155472")
+    @Test
+    public void getManagedService() {
+        String serviceInstanceName = getServiceInstanceName();
+
+        this.cloudFoundryOperations.services()
+            .createInstance(CreateServiceInstanceRequest.builder()
+                .planName("shared-vm") //TODO: Replace with derived value
+                .serviceName("p-redis") //TODO: Replace with derived value
+                .serviceInstanceName(serviceInstanceName)
+                .build())
+            .after(() -> this.cloudFoundryOperations.services()
+                .getInstance(GetServiceInstanceRequest.builder()
+                    .name(serviceInstanceName)
+                    .build()))
+            .map(ServiceInstance::getServiceInstance)
+            .subscribe(testSubscriber()
+                .assertEquals(serviceInstanceName));
+    }
+
+}
