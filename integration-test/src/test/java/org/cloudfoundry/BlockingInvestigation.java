7a1e10b3e6a1934b791b910f37e3cc0a2964601f
Ben Hale <bhale@pivotal.io>
2016-04-05 17:29:57 -0700

    Move Docker Image

---
+++ integration-test/src/test/java/org/cloudfoundry/BlockingInvestigation.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry;
+
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.SchedulerGroup;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public final class BlockingInvestigation {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        CompletableFuture<String> providedLater = new CompletableFuture<>();
+
+        Mono<String> result = Mono.defer(() -> Mono.fromFuture(providedLater))
+            .retry()
+            .cache();
+
+        System.out.println("Before consuming");
+        result
+            .publishOn(SchedulerGroup.async())
+            .consume(System.out::println);
+        System.out.println("After consuming");
+
+        providedLater.complete("foo");
+
+    }
+
+}
