b35cef4064abf8e2a42aaf2a8dafb7e7ef907f48
Ben Hale <bhale@pivotal.io>
2016-04-08 13:15:25 -0700

    Improve Release Script
    This change ensures that the created tag is signed.

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -9,7 +9,7 @@ mvn versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
 git add .
 git commit --message "v$RELEASE Release"
 
-git tag v$RELEASE
+git tag -s v$RELEASE -m "v$RELEASE"
 git reset --hard HEAD^1
 
 mvn versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
