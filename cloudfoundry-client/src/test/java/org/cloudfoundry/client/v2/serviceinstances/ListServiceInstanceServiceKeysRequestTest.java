dd56ec31f87595791ee6a40c119c4ab6e82753c1
Ben Hale <bhale@pivotal.io>
2016-05-04 06:52:26 -0700

    Fix naming typo

--- cloudfoundry-client/src/test/java/org/cloudfoundry/client/v2/serviceinstances/ListServiceInstanceServiceKeysRequestTest.java
+++ cloudfoundry-client/src/test/java/org/cloudfoundry/client/v2/serviceinstances/ListServiceInstanceServiceKeysRequestTest.java
@@ -23,7 +23,7 @@ import static org.cloudfoundry.ValidationResult.Status.INVALID;
 import static org.cloudfoundry.ValidationResult.Status.VALID;
 import static org.junit.Assert.assertEquals;
 
-public final class ListServiceInstancecServiceKeysRequestTest {
+public final class ListServiceInstanceServiceKeysRequestTest {
 
     @Test
     public void isInValidNoId() {
