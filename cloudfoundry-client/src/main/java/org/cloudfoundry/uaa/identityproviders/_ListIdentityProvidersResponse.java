8ea2b230e61c3a356d72f0accedddda910dca7cf
Ben Einaudi <benjamin.einaudi@orange.com>
2016-06-07 17:52:08 +0200

    List Identity Providers This change adds the api to list identity providers (GET /identity-providers) [#114246011][#497]

---
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/uaa/identityproviders/_ListIdentityProvidersResponse.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.uaa.identityproviders;
+
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import org.immutables.value.Value;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * The response from the list identity providers request
+ */
+@JsonDeserialize(using = _ListIdentityProvidersResponse.ListIdentityProvidersResponseDeserializer.class)
+@Value.Immutable
+abstract class _ListIdentityProvidersResponse {
+
+    /**
+     * The identity providers
+     */
+    abstract List<IdentityProvider> getIdentityProviders();
+
+    static final class ListIdentityProvidersResponseDeserializer extends StdDeserializer<ListIdentityProvidersResponse> {
+
+        private static final long serialVersionUID = 3381296378901733925L;
+
+        ListIdentityProvidersResponseDeserializer() {
+            super(ListIdentityProvidersResponse.class);
+        }
+
+        @Override
+        public ListIdentityProvidersResponse deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return ListIdentityProvidersResponse.builder()
+                .identityProviders(p.readValueAs(new TypeReference<List<IdentityProvider>>() {
+
+                }))
+                .build();
+        }
+
+    }
+
+}
