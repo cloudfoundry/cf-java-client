33f2224b893fe3034d336bd84f22bb350aa0ceb7
Ben Hale <bhale@pivotal.io>
2016-12-20 10:49:37 -0800

    Remove more Reactor Netty Error Handler
    Previously, only client error (4XX) handling was disabled.  This change
    disables server error (5XX) handling as well, allowing us to deal with a wider
    range of exceptions.

--- cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/client/v2/AbstractClientV2Operations.java
+++ cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/client/v2/AbstractClientV2Operations.java
@@ -38,44 +38,45 @@ public abstract class AbstractClientV2Operations extends AbstractReactorOperatio
     }
 
     protected final <T> Mono<T> delete(Object request, Class<T> responseType, Function<UriComponentsBuilder, UriComponentsBuilder> uriTransformer) {
-        return doDelete(request, responseType, getUriAugmenter(request, uriTransformer), outbound -> outbound.failOnClientError(false),
+        return doDelete(request, responseType, getUriAugmenter(request, uriTransformer), outbound -> outbound.failOnClientError(false).failOnServerError(false),
             ClientErrorMapper.cloudFoundry(this.connectionContext.getObjectMapper()));
     }
 
     protected final <T> Mono<T> get(Object request, Class<T> responseType, Function<UriComponentsBuilder, UriComponentsBuilder> uriTransformer) {
-        return doGet(responseType, getUriAugmenter(request, uriTransformer), outbound -> outbound.failOnClientError(false), ClientErrorMapper.cloudFoundry(this.connectionContext.getObjectMapper()));
+        return doGet(responseType, getUriAugmenter(request, uriTransformer), outbound -> outbound.failOnClientError(false).failOnServerError(false),
+            ClientErrorMapper.cloudFoundry(this.connectionContext.getObjectMapper()));
     }
 
     protected final Mono<HttpClientResponse> get(Object request, Function<UriComponentsBuilder, UriComponentsBuilder> uriTransformer) {
-        return get(request, uriTransformer, outbound -> outbound.failOnClientError(false));
+        return get(request, uriTransformer, outbound -> outbound.failOnClientError(false).failOnServerError(false));
     }
 
     protected final Mono<HttpClientResponse> get(Object request, Function<UriComponentsBuilder, UriComponentsBuilder> uriTransformer,
                                                  Function<HttpClientRequest, HttpClientRequest> requestTransformer) {
 
-        return doGet(getUriAugmenter(request, uriTransformer), outbound -> requestTransformer.apply(outbound.failOnClientError(false)),
+        return doGet(getUriAugmenter(request, uriTransformer), outbound -> requestTransformer.apply(outbound.failOnClientError(false).failOnServerError(false)),
             ClientErrorMapper.cloudFoundry(this.connectionContext.getObjectMapper()));
     }
 
     protected final <T> Mono<T> post(Object request, Class<T> responseType, Function<UriComponentsBuilder, UriComponentsBuilder> uriTransformer) {
-        return doPost(request, responseType, getUriAugmenter(request, uriTransformer), outbound -> outbound.failOnClientError(false),
+        return doPost(request, responseType, getUriAugmenter(request, uriTransformer), outbound -> outbound.failOnClientError(false).failOnServerError(false),
             ClientErrorMapper.cloudFoundry(this.connectionContext.getObjectMapper()));
     }
 
     protected final <T> Mono<T> post(Object request, Class<T> responseType, Function<UriComponentsBuilder, UriComponentsBuilder> uriTransformer,
                                      Function<HttpClientRequest, Mono<Void>> requestTransformer) {
-        return doPost(responseType, getUriAugmenter(request, uriTransformer), outbound -> requestTransformer.apply(outbound.failOnClientError(false)),
+        return doPost(responseType, getUriAugmenter(request, uriTransformer), outbound -> requestTransformer.apply(outbound.failOnClientError(false).failOnServerError(false)),
             ClientErrorMapper.cloudFoundry(this.connectionContext.getObjectMapper()));
     }
 
     protected final <T> Mono<T> put(Object request, Class<T> responseType, Function<UriComponentsBuilder, UriComponentsBuilder> uriTransformer) {
-        return doPut(request, responseType, getUriAugmenter(request, uriTransformer), outbound -> outbound.failOnClientError(false),
+        return doPut(request, responseType, getUriAugmenter(request, uriTransformer), outbound -> outbound.failOnClientError(false).failOnServerError(false),
             ClientErrorMapper.cloudFoundry(this.connectionContext.getObjectMapper()));
     }
 
     protected final <T> Mono<T> put(Object request, Class<T> responseType, Function<UriComponentsBuilder, UriComponentsBuilder> uriTransformer,
                                     Function<HttpClientRequest, Mono<Void>> requestTransformer) {
-        return doPut(responseType, getUriAugmenter(request, uriTransformer), outbound -> requestTransformer.apply(outbound.failOnClientError(false)),
+        return doPut(responseType, getUriAugmenter(request, uriTransformer), outbound -> requestTransformer.apply(outbound.failOnClientError(false).failOnServerError(false)),
             ClientErrorMapper.cloudFoundry(this.connectionContext.getObjectMapper()));
     }
 
