a1842584408ec2c24009cc3f5412300dc72f5210
Ben Hale <bhale@pivotal.io>
2017-01-31 16:42:36 -0800

    Lazy and Shared Wordlists
    Previously, the wordlists used for pushing applications with random names was
    loaded eagerly and individually for each instance of them that were created.
    Since they were only rarely used anyway, this change ensures that they are
    lazily initialized and when they are, they are shared between all instances
    that might use them.  This should lower the amount of memory used in cases
    where the wordlists aren't used (most cases) and in cases where more than a
    single instance is created.
    
    [resolves #676]

--- cloudfoundry-operations/src/main/java/org/cloudfoundry/operations/applications/WordListRandomWords.java
+++ cloudfoundry-operations/src/main/java/org/cloudfoundry/operations/applications/WordListRandomWords.java
@@ -27,43 +27,41 @@ import java.util.stream.Stream;
 
 final class WordListRandomWords implements RandomWords {
 
-    private final List<String> adjectives;
-
-    private final List<String> nouns;
-
-    private final Random random;
-
-    WordListRandomWords() {
-        this(getWordList("adjectives.txt"), getWordList("nouns.txt"), new SecureRandom());
-    }
-
-    private WordListRandomWords(List<String> adjectives, List<String> nouns, Random random) {
-        this.adjectives = adjectives;
-        this.nouns = nouns;
-        this.random = random;
-    }
+    private static final Random RANDOM = new SecureRandom();
 
     @Override
     public String getAdjective() {
-        return this.adjectives.get(this.random.nextInt(this.adjectives.size()));
+        return Adjectives.WORDS.get(RANDOM.nextInt(Adjectives.WORDS.size()));
     }
 
     @Override
     public String getNoun() {
-        return this.nouns.get(this.random.nextInt(this.nouns.size()));
+        return Nouns.WORDS.get(RANDOM.nextInt(Nouns.WORDS.size()));
     }
 
-    private static BufferedReader getFileReader(String filename) {
-        InputStream inputStream = WordListRandomWords.class.getClassLoader().getResourceAsStream(filename);
+    private static BufferedReader getReader(String resourceName) {
+        InputStream inputStream = WordListRandomWords.class.getClassLoader().getResourceAsStream(resourceName);
         return new BufferedReader(new InputStreamReader(inputStream));
     }
 
-    private static List<String> getWordList(String filename) {
-        try (Stream<String> stream = getFileReader(filename).lines()) {
+    private static List<String> getWordList(String resourceName) {
+        try (Stream<String> stream = getReader(resourceName).lines()) {
             return stream
                 .map(String::trim)
                 .collect(Collectors.toList());
         }
+    }
+
+    private static final class Adjectives {
+
+        private static List<String> WORDS = getWordList("adjectives.txt");
+
+    }
+
+    private static final class Nouns {
+
+        private static List<String> WORDS = getWordList("nouns.txt");
+
     }
 
 }
