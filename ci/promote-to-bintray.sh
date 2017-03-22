16cfb9c356835ccfde21efbaac8ce21a963de13a
Ben Hale <bhale@pivotal.io>
2017-03-22 09:08:01 -0700

    Maybe, possibly solved the release script failures

--- ci/promote-to-bintray.sh
+++ ci/promote-to-bintray.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-set -e -x
+set -e
 
 NAME=$(jq -r .name build-info/build-info.json)
 NUMBER=$(jq -r .number build-info/build-info.json)
 
 ## Promote to Bintray
-curl -X "POST" "https://repo.spring.io/api/build/distribute/$NAME/$NUMBER" \
-     -H "Content-Type: application/json; charset=utf-8" \
-     -u $ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD \
-     -d $"{ \"sourceRepos\": [ \"$SOURCE_REPOSITORY\" ], \"targetRepo\": \"$TARGET_REPOSITORY\" }" \
+curl --request "POST" "https://repo.spring.io/api/build/distribute/$NAME/$NUMBER" \
+     --header "Content-Type: application/json; charset=utf-8" \
+     --user $ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD \
+     --data "{ \"sourceRepos\": [ \"$SOURCE_REPOSITORY\" ], \"targetRepo\": \"$TARGET_REPOSITORY\" }" \
      --fail
--- ci/promote-to-maven-central.sh
+++ ci/promote-to-maven-central.sh
@@ -6,8 +6,8 @@ GROUP_ID=$(jq -r .modules[0].id build-info/build-info.json | cut -d ':' -f 1)
 VERSION=$(jq -r .modules[0].id build-info/build-info.json | cut -d ':' -f 3)
 
 ## Promote to Maven Central
-curl -X "POST" "https://api.bintray.com/maven_central_sync/spring/jars/$GROUP_ID/versions/$VERSION" \
-     -H "Content-Type: application/json; charset=utf-8" \
-     -u $BINTRAY_USERNAME:$BINTRAY_API_KEY \
-     -d $"{ \"username\": \"$MAVEN_CENTRAL_USERNAME\", \"password\": \"$MAVEN_CENTRAL_PASSWORD\" }" \
+curl --request "POST" "https://api.bintray.com/maven_central_sync/spring/jars/$GROUP_ID/versions/$VERSION" \
+     --header "Content-Type: application/json; charset=utf-8" \
+     --user $BINTRAY_USERNAME:$BINTRAY_API_KEY \
+     --data "{ \"username\": \"$MAVEN_CENTRAL_USERNAME\", \"password\": \"$MAVEN_CENTRAL_PASSWORD\" }" \
      --fail
