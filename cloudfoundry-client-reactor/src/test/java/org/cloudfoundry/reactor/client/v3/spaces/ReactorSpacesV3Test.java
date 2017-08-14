3f648932d93b9d3d861ba8517731a8912aac309f
Ben Hale <bhale@pivotal.io>
2017-08-14 10:53:52 -0700

    Missing V3 Tests
    This change finishes off the V3 updates by adding a few missing tests.

--- cloudfoundry-client-reactor/src/test/java/org/cloudfoundry/reactor/client/v3/spaces/ReactorSpacesV3Test.java
+++ cloudfoundry-client-reactor/src/test/java/org/cloudfoundry/reactor/client/v3/spaces/ReactorSpacesV3Test.java
@@ -23,6 +23,8 @@ import org.cloudfoundry.client.v3.spaces.AssignSpaceIsolationSegmentRequest;
 import org.cloudfoundry.client.v3.spaces.AssignSpaceIsolationSegmentResponse;
 import org.cloudfoundry.client.v3.spaces.GetSpaceIsolationSegmentRequest;
 import org.cloudfoundry.client.v3.spaces.GetSpaceIsolationSegmentResponse;
+import org.cloudfoundry.client.v3.spaces.GetSpaceRequest;
+import org.cloudfoundry.client.v3.spaces.GetSpaceResponse;
 import org.cloudfoundry.client.v3.spaces.ListSpacesRequest;
 import org.cloudfoundry.client.v3.spaces.ListSpacesResponse;
 import org.cloudfoundry.client.v3.spaces.SpaceResource;
@@ -73,6 +75,36 @@ public class ReactorSpacesV3Test extends AbstractClientApiTest {
                     .build())
                 .link("related", Link.builder()
                     .href("https://api.example.org/v3/isolation_segments/e4c91047-3b29-4fda-b7f9-04033e5a9c9f")
+                    .build())
+                .build())
+            .expectComplete()
+            .verify(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void get() {
+        mockRequest(InteractionContext.builder()
+            .request(TestRequest.builder()
+                .method(GET).path("/v3/spaces/test-space-id")
+                .build())
+            .response(TestResponse.builder()
+                .status(OK)
+                .payload("fixtures/client/v3/spaces/GET_{id}_response.json")
+                .build())
+            .build());
+
+        this.spaces
+            .get(GetSpaceRequest.builder()
+                .spaceId("test-space-id")
+                .build())
+            .as(StepVerifier::create)
+            .expectNext(GetSpaceResponse.builder()
+                .id("885735b5-aea4-4cf5-8e44-961af0e41920")
+                .createdAt("2017-02-01T01:33:58Z")
+                .updatedAt("2017-02-01T01:33:58Z")
+                .name("space1")
+                .link("self", Link.builder()
+                    .href("https://api.example.org/v3/spaces/885735b5-aea4-4cf5-8e44-961af0e41920")
                     .build())
                 .build())
             .expectComplete()
