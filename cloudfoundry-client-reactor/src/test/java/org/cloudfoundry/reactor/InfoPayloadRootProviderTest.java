24a872784aa5c0317acbe24013b3825b47b83451
Ben Hale <bhale@pivotal.io>
2017-08-24 14:48:29 -0700

    Extract Endpoints from Endpoint Root
    Previously all endpoints were extracted from the /v2/info endpoint. With the
    forthcoming NetworkingClient, we were looking at a client that was not listed
    in that endpoint. This change adds a RootPayloadRootProvider to look to the
    new location for this information. For backwards compatibility we've also
    introduced a new DelegatingRootProvider that starts by looking at the root
    payload and falls back to the info payload if a key is not found.
    
    [#801]

---
+++ cloudfoundry-client-reactor/src/test/java/org/cloudfoundry/reactor/InfoPayloadRootProviderTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2013-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.reactor;
+
+import org.junit.Test;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+import static io.netty.handler.codec.http.HttpMethod.GET;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+
+public final class InfoPayloadRootProviderTest extends AbstractRestTest {
+
+    private final InfoPayloadRootProvider rootProvider = InfoPayloadRootProvider.builder()
+        .apiHost("localhost")
+        .port(this.mockWebServer.getPort())
+        .secure(false)
+        .objectMapper(CONNECTION_CONTEXT.getObjectMapper())
+        .build();
+
+    @Test
+    public void getRoot() {
+        this.rootProvider
+            .getRoot(CONNECTION_CONTEXT)
+            .as(StepVerifier::create)
+            .expectNext(String.format("http://localhost:%d", this.mockWebServer.getPort()))
+            .expectComplete()
+            .verify(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void getRootKey() {
+        mockRequest(InteractionContext.builder()
+            .request(TestRequest.builder()
+                .method(GET).path("/v2/info")
+                .build())
+            .response(TestResponse.builder()
+                .status(OK)
+                .payload("fixtures/client/v2/info/GET_response.json")
+                .build())
+            .build());
+
+        this.rootProvider
+            .getRoot("authorization_endpoint", CONNECTION_CONTEXT)
+            .as(StepVerifier::create)
+            .expectNext("http://localhost:8080/uaa")
+            .expectComplete()
+            .verify(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void getRootKeyNoKey() {
+        mockRequest(InteractionContext.builder()
+            .request(TestRequest.builder()
+                .method(GET).path("/v2/info")
+                .build())
+            .response(TestResponse.builder()
+                .status(OK)
+                .payload("fixtures/client/v2/info/GET_response.json")
+                .build())
+            .build());
+
+        this.rootProvider
+            .getRoot("invalid-key", CONNECTION_CONTEXT)
+            .as(StepVerifier::create)
+            .expectError(IllegalArgumentException.class)
+            .verify(Duration.ofSeconds(5));
+    }
+
+}
---
+++ cloudfoundry-client-reactor/src/test/java/org/cloudfoundry/reactor/RootPayloadRootProviderTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2013-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.reactor;
+
+import org.junit.Test;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+import static io.netty.handler.codec.http.HttpMethod.GET;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+
+public final class RootPayloadRootProviderTest extends AbstractRestTest {
+
+    private final RootPayloadRootProvider rootProvider = RootPayloadRootProvider.builder()
+        .apiHost("localhost")
+        .port(this.mockWebServer.getPort())
+        .secure(false)
+        .objectMapper(CONNECTION_CONTEXT.getObjectMapper())
+        .build();
+
+    @Test
+    public void getRoot() {
+        this.rootProvider
+            .getRoot(CONNECTION_CONTEXT)
+            .as(StepVerifier::create)
+            .expectNext(String.format("http://localhost:%d", this.mockWebServer.getPort()))
+            .expectComplete()
+            .verify(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void getRootKey() {
+        mockRequest(InteractionContext.builder()
+            .request(TestRequest.builder()
+                .method(GET).path("/")
+                .build())
+            .response(TestResponse.builder()
+                .status(OK)
+                .payload("fixtures/GET_response.json")
+                .build())
+            .build());
+
+        this.rootProvider
+            .getRoot("cloud_controller_v2", CONNECTION_CONTEXT)
+            .as(StepVerifier::create)
+            .expectNext("https://api.run.pivotal.io/v2")
+            .expectComplete()
+            .verify(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void getRootKeyNoKey() {
+        mockRequest(InteractionContext.builder()
+            .request(TestRequest.builder()
+                .method(GET).path("/")
+                .build())
+            .response(TestResponse.builder()
+                .status(OK)
+                .payload("fixtures/GET_response.json")
+                .build())
+            .build());
+
+        this.rootProvider
+            .getRoot("invalid-key", CONNECTION_CONTEXT)
+            .as(StepVerifier::create)
+            .expectError(IllegalArgumentException.class)
+            .verify(Duration.ofSeconds(5));
+    }
+
+}
