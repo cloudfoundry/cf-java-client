b4f40c3fe7bb680b24faf79eef49765779822604
Encho Belezirev <encho.belezirev@gmail.com>
2019-10-24 12:58:12 +0300

    Fix scale application request in Applications V3 client

--- cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/client/v3/applications/ReactorApplicationsV3.java
+++ cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/client/v3/applications/ReactorApplicationsV3.java
@@ -62,6 +62,7 @@ import org.cloudfoundry.client.v3.applications.UpdateApplicationResponse;
 import org.cloudfoundry.reactor.ConnectionContext;
 import org.cloudfoundry.reactor.TokenProvider;
 import org.cloudfoundry.reactor.client.v3.AbstractClientV3Operations;
+
 import reactor.core.publisher.Mono;
 
 /**
@@ -172,7 +173,7 @@ public final class ReactorApplicationsV3 extends AbstractClientV3Operations impl
 
     @Override
     public Mono<ScaleApplicationResponse> scale(ScaleApplicationRequest request) {
-        return put(request, ScaleApplicationResponse.class, builder -> builder.pathSegment("apps", request.getApplicationId(), "processes", request.getType(), "actions", "scale"))
+        return post(request, ScaleApplicationResponse.class, builder -> builder.pathSegment("apps", request.getApplicationId(), "processes", request.getType(), "actions", "scale"))
             .checkpoint();
     }
 
