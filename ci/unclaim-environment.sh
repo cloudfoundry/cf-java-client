e35aef88fd8b320513f3756b6d7df692c849dc84
Daniel Mikusa <dmikusa@vmware.com>
2021-05-03 09:43:09 -0400

    Fixed path to claim creds

--- ci/unclaim-environment.sh
+++ ci/unclaim-environment.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")"/../..)
 
-CREDENTIALS=$(cat "${ROOT}"/environment/credentials.json)
+CREDENTIALS=$(cat "${ROOT}"/environment/cf-creds.json)
 
 printf "Unclaiming %s\n" "$(jq -n -r --argjson credentials "${CREDENTIALS}" '$credentials.name')"
 
