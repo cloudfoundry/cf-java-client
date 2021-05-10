81ea15107cd27679a16346d80d02ef69c182aced
Daniel Mikusa <dmikusa@vmware.com>
2021-05-10 16:21:00 -0400

    Removing Bintray & using https://github.com/spring-io/concourse-release-scripts to sync with Maven central

--- ci/promote-to-maven-central.sh
+++ ci/promote-to-maven-central.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-GROUP_ID=$(jq -r .modules[0].id build-info/build-info.json | cut -d ':' -f 1)
-VERSION=$(jq -r .modules[0].id build-info/build-info.json | cut -d ':' -f 3)
+java -jar /opt/spring-boot-release-scripts.jar publishToCentral 'RELEASE' build-info/build-info.json artifactory-repo
 
-## Promote to Maven Central
-curl --request "POST" "https://api.bintray.com/maven_central_sync/spring/jars/$GROUP_ID/versions/$VERSION" \
-     --header "Content-Type: application/json; charset=utf-8" \
-     --user $BINTRAY_USERNAME:$BINTRAY_API_KEY \
-     --data "{ \"username\": \"$MAVEN_CENTRAL_USERNAME\", \"password\": \"$MAVEN_CENTRAL_PASSWORD\" }" \
-     --fail
+echo "Sync complete"
