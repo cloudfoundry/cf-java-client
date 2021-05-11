9c1e21b48fec2472796d8e8392771b8e27191175
Daniel Mikusa <dmikusa@vmware.com>
2021-05-11 00:45:25 -0400

    Updates to CI for promote to maven

--- ci/promote-to-maven-central.sh
+++ ci/promote-to-maven-central.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 export BUILD_INFO_LOCATION=$(pwd)/build-info/build-info.json
 
+ls -la "$(pwd)/build-info"
+cat "$BUILD_INFO_LOCATION"
+
 java -jar /opt/concourse-release-scripts.jar publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" artifactory-repo
 
 echo "Sync complete"
