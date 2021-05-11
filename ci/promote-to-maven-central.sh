f6c254d1dbd0faca2592ec0ae041dd5912e6b410
Daniel Mikusa <dmikusa@vmware.com>
2021-05-11 08:23:57 -0400

    Switch to pulling build-info from artifactory input

--- ci/promote-to-maven-central.sh
+++ ci/promote-to-maven-central.sh
@@ -2,10 +2,7 @@
 
 set -euo pipefail
 
-export BUILD_INFO_LOCATION=$(pwd)/build-info/build-info.json
-
-ls -la "$(pwd)/build-info"
-cat "$BUILD_INFO_LOCATION"
+export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
 java -jar /opt/concourse-release-scripts.jar publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" artifactory-repo
 
