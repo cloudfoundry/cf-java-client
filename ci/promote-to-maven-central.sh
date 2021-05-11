50405607d72b65bd028a51faf191aff526195e54
Daniel Mikusa <dmikusa@vmware.com>
2021-05-11 08:30:59 -0400

    Updates dockerfile & maven promotion scripts

--- ci/promote-to-maven-central.sh
+++ ci/promote-to-maven-central.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export BUILD_INFO_LOCATION=$(pwd)/build-info/build-info.json
+export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
 java -jar /opt/concourse-release-scripts.jar publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" artifactory-repo
 
