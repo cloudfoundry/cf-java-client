db76695fea2aa2e7f3c3772c25ff1d2cb38eb992
David O'Sullivan <davidos@vmware.com>
2022-05-05 21:08:46 +0100

    CC API version bump and necessary changes for support

--- cloudfoundry-client/src/main/java/org/cloudfoundry/client/CloudFoundryClient.java
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/client/CloudFoundryClient.java
@@ -79,7 +79,7 @@ public interface CloudFoundryClient {
     /**
      * The currently supported Cloud Controller API version
      */
-    String SUPPORTED_API_VERSION = "2.164.0";
+    String SUPPORTED_API_VERSION = "2.180.0";
 
     /**
      * Main entry point to the Cloud Foundry Application Usage Events Client API
