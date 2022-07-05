447d64410b2187ca5e9cba97f20f370e01299d36
theghost5800 <kristian0001@gmail.com>
2022-07-05 17:48:51 +0300

    Allow null values in map of service parameters

--- cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/serviceinstances/_GetUserProvidedCredentialsResponse.java
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/serviceinstances/_GetUserProvidedCredentialsResponse.java
@@ -2,6 +2,7 @@ package org.cloudfoundry.client.v3.serviceinstances;
 
 import java.util.Map;
 
+import org.cloudfoundry.AllowNulls;
 import org.immutables.value.Value;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
@@ -16,5 +17,6 @@ abstract class _GetUserProvidedCredentialsResponse {
      * @return credentials as map
      */
     @JsonAnyGetter
+    @AllowNulls
     abstract Map<String, Object> getCredentials();
 }
