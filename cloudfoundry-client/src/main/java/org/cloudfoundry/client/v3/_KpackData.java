7fb6fe9b9ad0125b15751b3e18dee046ae91d0a9
Daniel Mikusa <dmikusa@vmware.com>
2022-07-27 15:41:57 -0400

    Add LifecycleType for KPACK (#1162)
    There are presently three LifecycleTypes supported: buildpack, docker and kpack. The first two have been supported, and this PR adds support for the third. It also includes a KpackData class for the lifecycle data that the kpack lifecycle type can return.
    
    API documentation clarifies what is allowed:
    
    http://v3-apidocs.cloudfoundry.org/version/3.122.0/index.html#lifecycles
    
    Includes a test which expands and deserializes the Kpack LifecycleType.
    
    Resolves #1094
    
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>

--- cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/_KpackData.java
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/_KpackData.java
@@ -17,34 +17,24 @@
 package org.cloudfoundry.client.v3;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.cloudfoundry.Nullable;
 import org.immutables.value.Value;
 
+import java.util.List;
+
 /**
- * The lifecycle type
+ * Data type for the Kpack
  */
 @JsonDeserialize
 @Value.Immutable
-abstract class _Lifecycle {
+abstract class _KpackData implements LifecycleData {
 
     /**
-     * The data for the lifecycle
+     * The buildpack
      */
-    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "type")
-    @JsonSubTypes({
-        @JsonSubTypes.Type(name = "buildpack", value = BuildpackData.class),
-        @JsonSubTypes.Type(name = "docker", value = DockerData.class),
-        @JsonSubTypes.Type(name = "kpack", value = KpackData.class)
-    })
-    @JsonProperty("data")
-    abstract LifecycleData getData();
-
-    /**
-     * The type
-     */
-    @JsonProperty("type")
-    abstract LifecycleType getType();
+    @JsonProperty("buildpacks")
+    @Nullable
+    abstract List<String> getBuildpacks();
 
 }
