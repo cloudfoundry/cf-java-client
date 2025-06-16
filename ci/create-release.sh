eb80787bedd0e183c90f19200a262779ce6a475c
Daniel Garnier-Moiroux <git@garnier.wf>
2025-06-16 18:04:31 +0200

    v5.14.0.RELEASE Release

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -8,7 +8,7 @@ SNAPSHOT=$2
 ./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
 git add .
 git commit --message "v$RELEASE Release"
-git tag -s v$RELEASE -m "v$RELEASE"
+git tag v$RELEASE -m "v$RELEASE"
 
 git reset --hard HEAD^1
 ./mvnw versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
