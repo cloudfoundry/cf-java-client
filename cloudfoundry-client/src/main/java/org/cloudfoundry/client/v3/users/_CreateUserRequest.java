0a433f8a72d363fb739b549548abac92f1c6b3be
Merchant, Saifuddin A <saifuddin.a.merchant@chase.com>
2025-12-23 12:22:34 -0700

    Implement the User Version 3 API along with Unit Test

---
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/users/_CreateUserRequest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2013-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cloudfoundry.client.v3.users;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.cloudfoundry.Nullable;
+import org.cloudfoundry.client.v3.Metadata;
+import org.immutables.value.Value;
+
+/**
+ * The request payload for the Create User operation
+ */
+@JsonSerialize
+@Value.Immutable
+abstract class _CreateUserRequest {
+
+    /**
+     * Unique identifier for the user. For UAA users this will match the UAA user ID; in the case of UAA clients, this will match the UAA client ID
+     */
+    @JsonProperty("guid")
+    abstract String getUserId();
+
+    /**
+     * Username of the user to be created. This can only be provided together with origin.
+     */
+    @JsonProperty("username")
+    @Nullable
+    abstract String getUsername();
+
+    /**
+     * Origin of the user to be created. This can only be provided together with username and cannot be uaa.
+     */
+    @JsonProperty("origin")
+    @Nullable
+    abstract String getOrigin();
+
+    /**
+     * The metadata Labels and Annotations applied to the user
+     */
+    @JsonProperty("metadata")
+    @Nullable
+    abstract Metadata getMetadata();
+}
