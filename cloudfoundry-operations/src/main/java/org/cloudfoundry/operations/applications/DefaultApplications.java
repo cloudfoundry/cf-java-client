6f43cf001765782b80bfed8834f0e49e1d01cadd
Daniel Garnier-Moiroux <daniel.garnier-moiroux@broadcom.com>
2026-01-02 12:21:59 +0100

    DefaultApplications: rename uses v3 api

--- cloudfoundry-operations/src/main/java/org/cloudfoundry/operations/applications/DefaultApplications.java
+++ cloudfoundry-operations/src/main/java/org/cloudfoundry/operations/applications/DefaultApplications.java
@@ -552,10 +552,12 @@ public final class DefaultApplications implements Applications {
 
     @Override
     public Mono<Void> rename(RenameApplicationRequest request) {
-        return getApplicationId(request.getName())
+        return getApplicationIdV3(request.getName())
                 .flatMap(
                         applicationId ->
-                                requestUpdateApplicationName(applicationId, request.getNewName()))
+                                requestUpdateApplicationV3(
+                                        applicationId,
+                                        builder -> builder.name(request.getNewName())))
                 .then()
                 .transform(OperationsLogging.log("Rename Application"))
                 .checkpoint();
@@ -2161,6 +2163,21 @@ public final class DefaultApplications implements Applications {
                                                 .applicationId(applicationId))
                                 .build())
                 .cast(AbstractApplicationResource.class);
+    }
+
+    private Mono<Void> requestUpdateApplicationV3(
+            String applicationId,
+            UnaryOperator<org.cloudfoundry.client.v3.applications.UpdateApplicationRequest.Builder>
+                    modifier) {
+        return this.cloudFoundryClient
+                .applicationsV3()
+                .update(
+                        modifier.apply(
+                                        org.cloudfoundry.client.v3.applications
+                                                .UpdateApplicationRequest.builder()
+                                                .applicationId(applicationId))
+                                .build())
+                .then();
     }
 
     private Mono<AbstractApplicationResource> requestUpdateApplicationEnvironment(
