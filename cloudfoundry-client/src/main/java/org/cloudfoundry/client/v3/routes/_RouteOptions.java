584a217ffea5266deec1d8f3254a3bcafc23da2d
theghost5800 <kristian0001@gmail.com>
2026-03-12 18:03:52 +0200

    Create/Update route options
    Refactor _RouteOptions class to handle added options in future
    
    (cherry picked from commit 87064a83c4f6c6ff1fccc29ecdeb8d0c224384d0)

--- cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/routes/_RouteOptions.java
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/routes/_RouteOptions.java
@@ -1,8 +1,14 @@
 package org.cloudfoundry.client.v3.routes;
 
+import java.util.Map;
+import java.util.Optional;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import org.cloudfoundry.Nullable;
+import org.cloudfoundry.AllowNulls;
 import org.immutables.value.Value;
 
 /**
@@ -13,10 +19,40 @@ import org.immutables.value.Value;
 abstract class _RouteOptions {
 
     /**
-     * The loadbalancing
+     * All route options, including unknown future keys.
      */
-    @JsonProperty("loadbalancing")
-    @Nullable
-    public abstract String getLoadbalancing();
+    @JsonAnyGetter
+    @JsonProperty("options")
+    @JsonInclude(value = JsonInclude.Include.ALWAYS)
+    @AllowNulls
+    public abstract Map<String, Object> getValues();
+
+    @JsonIgnore
+    @Value.Derived
+    public Optional<String> getLoadbalancing() {
+        return getString("loadbalancing");
+    }
+
+    @JsonIgnore
+    @Value.Derived
+    public Optional<String> getHashHeader() {
+        return getString("hash_header");
+    }
+
+    @JsonIgnore
+    @Value.Derived
+    public Optional<String> getHashBalance() {
+        return getString("hash_balance");
+    }
+
+    @JsonIgnore
+    public Optional<Object> get(String key) {
+        return Optional.ofNullable(getValues().get(key));
+    }
+
+    private Optional<String> getString(String key) {
+        Object value = getValues().get(key);
+        return value instanceof String ? Optional.of((String) value) : Optional.empty();
+    }
 
 }
