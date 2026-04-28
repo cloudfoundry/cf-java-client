98a3de24e1f3d4c55587df406e26d0589767c4a5
Georg Lokowandt <georg.lokowandt@sap.com>
2026-04-28 14:12:07 +0200

    make compatibilityChecker robust if v2 api is disabled

--- cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/client/CloudFoundryClientCompatibilityChecker.java
+++ cloudfoundry-client-reactor/src/main/java/org/cloudfoundry/reactor/client/CloudFoundryClientCompatibilityChecker.java
@@ -20,27 +20,30 @@ import static org.cloudfoundry.util.tuple.TupleUtils.consumer;
 
 import com.github.zafarkhaja.semver.Version;
 import org.cloudfoundry.client.CloudFoundryClient;
+import org.cloudfoundry.client.GetRootRequest;
+import org.cloudfoundry.client.Root;
 import org.cloudfoundry.client.v2.info.GetInfoRequest;
 import org.cloudfoundry.client.v2.info.Info;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 final class CloudFoundryClientCompatibilityChecker {
 
     private final Logger logger = LoggerFactory.getLogger("cloudfoundry-client.compatibility");
 
     private final Info info;
+    private final Root root;
 
-    CloudFoundryClientCompatibilityChecker(Info info) {
+    CloudFoundryClientCompatibilityChecker(Info info, Root root) {
         this.info = info;
+        this.root = root;
     }
 
     void check() {
-        this.info
-                .get(GetInfoRequest.builder().build())
-                .map(response -> Version.valueOf(response.getApiVersion()))
-                .zipWith(Mono.just(Version.valueOf(CloudFoundryClient.SUPPORTED_API_VERSION)))
+        Mono<Tuple2<Version, Version>> v2 = checkV2();
+        v2.switchIfEmpty(checkV3())
                 .subscribe(
                         consumer(
                                 (server, supported) ->
@@ -51,11 +54,41 @@ final class CloudFoundryClientCompatibilityChecker {
                                         t));
     }
 
+    private Mono<Tuple2<Version, Version>> checkV2() {
+        return this.info
+                .get(GetInfoRequest.builder().build())
+                .flatMap(
+                        response -> {
+                            String version = response.getApiVersion();
+                            if (version == null || version.isEmpty()) {
+                                if ("CF API v2 is disabled".equals(response.getSupport())) {
+                                    this.logger.warn(
+                                            "calling v2 info endpoint but CF API v2 is disabled",
+                                            response);
+                                }
+                                return Mono.empty();
+                            } else {
+                                return Mono.just(Version.valueOf(version));
+                            }
+                        })
+                .zipWith(Mono.just(Version.valueOf(CloudFoundryClient.SUPPORTED_API_VERSION)));
+    }
+
+    private Mono<Tuple2<Version, Version>> checkV3() {
+        return this.root
+                .get(GetRootRequest.builder().build())
+                .map(
+                        response -> {
+                            String versionV3 = response.getApiVersionV3();
+                            return Version.valueOf(versionV3);
+                        })
+                .zipWith(Mono.just(Version.valueOf(CloudFoundryClient.SUPPORTED_API_VERSION_V3)));
+    }
+
     private static void logCompatibility(Version server, Version supported, Logger logger) {
         String message =
                 "Client supports API version {} and is connected to server with API version {}."
                         + " Things may not work as expected.";
-
         if (server.greaterThan(supported)) {
             logger.info(message, supported, server);
         } else if (server.lessThan(supported)) {
