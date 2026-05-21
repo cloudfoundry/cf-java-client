43817abd7736fcc8183bdcffddc83a20e70ebb82
theghost5800 <kristian0001@gmail.com>
2026-05-21 15:19:32 +0300

    Switch RouteOptions to use specific fields for every parameter
    (cherry picked from commit c4298c9fd698fb4945ad1205b614935c969bef9b)

--- cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/routes/_RouteOptions.java
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/routes/_RouteOptions.java
@@ -1,14 +1,9 @@
 package org.cloudfoundry.client.v3.routes;
 
-import java.util.Map;
-import java.util.Optional;
-
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import org.cloudfoundry.AllowNulls;
+import org.cloudfoundry.Nullable;
 import org.immutables.value.Value;
 
 /**
@@ -19,40 +14,26 @@ import org.immutables.value.Value;
 abstract class _RouteOptions {
 
     /**
-     * All route options, including unknown future keys.
+     * The loadbalancing algorithm
      */
-    @JsonAnyGetter
-    @JsonProperty("options")
-    @JsonInclude(value = JsonInclude.Include.ALWAYS)
-    @AllowNulls
-    public abstract Map<String, Object> getValues();
+    @JsonInclude
+    @JsonProperty("loadbalancing")
+    @Nullable
+    public abstract String getLoadbalancing();
 
-    @JsonIgnore
-    @Value.Derived
-    public Optional<String> getLoadbalancing() {
-        return getString("loadbalancing");
-    }
+    /**
+     * The hash header
+     */
+    @JsonInclude
+    @JsonProperty("hash_header")
+    @Nullable
+    public abstract String getHashHeader();
 
-    @JsonIgnore
-    @Value.Derived
-    public Optional<String> getHashHeader() {
-        return getString("hash_header");
-    }
-
-    @JsonIgnore
-    @Value.Derived
-    public Optional<String> getHashBalance() {
-        return getString("hash_balance");
-    }
-
-    @JsonIgnore
-    public Optional<Object> get(String key) {
-        return Optional.ofNullable(getValues().get(key));
-    }
-
-    private Optional<String> getString(String key) {
-        Object value = getValues().get(key);
-        return value instanceof String ? Optional.of((String) value) : Optional.empty();
-    }
-
+    /**
+     * The hash balance
+     */
+    @JsonInclude
+    @JsonProperty("hash_balance")
+    @Nullable
+    public abstract String getHashBalance();
 }
